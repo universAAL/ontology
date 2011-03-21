c71a5181a6f1270e5bc75d732d1bb0e2df40ce28
Alvaro Fides <alfiva@itaca.upv.es>
2011-03-21 09:43:52 +0000

    Formatted as Java conventions (ref.D2.3A)

--- window/src/main/java/org/universAAL/ontology/window/Window.java
+++ window/src/main/java/org/universAAL/ontology/window/Window.java
@@ -16,7 +16,7 @@
 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 	See the License for the specific language governing permissions and
 	limitations under the License.
-*/
+ */
 
 package org.universAAL.ontology.window;
 
@@ -31,129 +31,133 @@ import org.universAAL.ontology.phThing.Device;
  * 
  * @author Steeven Zeiss
  * 
- *
+ * 
  */
 public class Window extends Device {
-	public static final String WINDOW_NAMESPACE = "http://ontology.persona.ima.igd.fhg.de/Window.owl#";
-	public static final String MY_URI;
-	public static final String PROP_HAS_TYPE;
-	public static final String PROP_WINDOW_LOCATION;
-	public static final String PROP_WINDOW_STATUS;
-	static {
-		MY_URI = WINDOW_NAMESPACE + "Window";
-		PROP_HAS_TYPE = WINDOW_NAMESPACE + "hasType";
-		PROP_WINDOW_LOCATION = WINDOW_NAMESPACE + "windowLocation";
-		PROP_WINDOW_STATUS  = WINDOW_NAMESPACE + "windowStatus";
-		register(Window.class);
-	}
-	
-	public static Restriction getClassRestrictionsOnProperty(String propURI) {
-		if (PROP_HAS_TYPE.equals(propURI))
-			return Restriction.getAllValuesRestrictionWithCardinality(propURI,
-					WindowType.MY_URI, 1, 1);
-		if (PROP_WINDOW_LOCATION.equals(propURI))
-			return Restriction.getAllValuesRestrictionWithCardinality(propURI,
-					AbsLocation.MY_URI, 1, 1);
-		if (PROP_WINDOW_STATUS.equals(propURI))
-			return OrderingRestriction.newOrderingRestriction(new Integer(100),
-					new Integer(0), true, true,
-					Restriction.getAllValuesRestrictionWithCardinality(propURI,
-							TypeMapper.getDatatypeURI(Integer.class), 1, 1));
-		return ManagedIndividual.getClassRestrictionsOnProperty(propURI);
-	}
-	
-	public static String[] getStandardPropertyURIs() {
-		return new String[] {
-				PROP_HAS_TYPE,
-				PROP_WINDOW_LOCATION,
-				PROP_WINDOW_STATUS
-		};
-	}
-	
-	public static String getRDFSComment() {
-		return "The class of all windows.";
-	}
-	
-	public static String getRDFSLabel() {
-		return "Window";
-	}
-	
-	public Window() {
-		super();
-	}
-	
-	public Window(String uri) {
-		super(uri);
-	}
+    public static final String WINDOW_NAMESPACE = "http://ontology.persona.ima.igd.fhg.de/Window.owl#";
+    public static final String MY_URI;
+    public static final String PROP_HAS_TYPE;
+    public static final String PROP_WINDOW_LOCATION;
+    public static final String PROP_WINDOW_STATUS;
+    static {
+	MY_URI = WINDOW_NAMESPACE + "Window";
+	PROP_HAS_TYPE = WINDOW_NAMESPACE + "hasType";
+	PROP_WINDOW_LOCATION = WINDOW_NAMESPACE + "windowLocation";
+	PROP_WINDOW_STATUS = WINDOW_NAMESPACE + "windowStatus";
+	register(Window.class);
+    }
 
-	public Window(String uri, AbsLocation loc) {
-		super(uri);
-		if (  loc == null)
-			throw new IllegalArgumentException();
-		props.put(PROP_WINDOW_LOCATION, loc);
-		props.put(PROP_WINDOW_STATUS, new Integer(0));
-	}
-	
-	public Window(String uri, WindowType type, AbsLocation loc) {
-		super(uri);
-		if (type == null  ||  loc == null)
-			throw new IllegalArgumentException();
-		
-		props.put(PROP_HAS_TYPE, type);
-		props.put(PROP_WINDOW_LOCATION, loc);
-	}
-	
-//	public Location getAmbientCoverage() {
-//		return (Location) props.get(PROP_AMBIENT_COVERAGE);
-//	}
-	
-/*	public int getBrightness() {
-		Integer i = (Integer) props.get(PROP_SOCKET_VALUE);
-		return (i == null)? -1 : i.intValue();
-	}*/
-	
-	public WindowType getWindowType() {
-		return (WindowType) props.get(PROP_HAS_TYPE);
-	}
-	
-	public AbsLocation getWindowLocation() {
-		return (AbsLocation) props.get(PROP_WINDOW_LOCATION);
-	}
-	
-//	public void setAmbientCoverage(Location l) {
-//		if (l != null)
-//			props.put(PROP_AMBIENT_COVERAGE, l);
-//	}
-	
-//	public void setBrightness(int percentage) {
-//		if (percentage > -1  &&  percentage < 101)
-//			props.put(PROP_SOCKET_VALUE, new Integer(percentage));
-//	}
-	
-	public void setWindowLocation(AbsLocation l) {
-		if (l != null)
-			props.put(PROP_WINDOW_LOCATION, l);
-	}
+    public static Restriction getClassRestrictionsOnProperty(String propURI) {
+	if (PROP_HAS_TYPE.equals(propURI))
+	    return Restriction.getAllValuesRestrictionWithCardinality(propURI,
+		    WindowType.MY_URI, 1, 1);
+	if (PROP_WINDOW_LOCATION.equals(propURI))
+	    return Restriction.getAllValuesRestrictionWithCardinality(propURI,
+		    AbsLocation.MY_URI, 1, 1);
+	if (PROP_WINDOW_STATUS.equals(propURI))
+	    return OrderingRestriction.newOrderingRestriction(new Integer(100),
+		    new Integer(0), true, true, Restriction
+			    .getAllValuesRestrictionWithCardinality(propURI,
+				    TypeMapper.getDatatypeURI(Integer.class),
+				    1, 1));
+	return ManagedIndividual.getClassRestrictionsOnProperty(propURI);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.universAAL.middleware.owl.ManagedIndividual#getPropSerializationType(java.lang.String)
-	 */
-	public int getPropSerializationType(String propURI) {
-		return  PROP_WINDOW_LOCATION.equals(propURI)?
-						PROP_SERIALIZATION_REDUCED : PROP_SERIALIZATION_FULL;
-		
-		//		return (PROP_AMBIENT_COVERAGE.equals(propURI)
-//				|| PROP_SOCKET_LOCATION.equals(propURI))?
-//						PROP_SERIALIZATION_REDUCED : PROP_SERIALIZATION_FULL;
-	}
+    public static String[] getStandardPropertyURIs() {
+	return new String[] { PROP_HAS_TYPE, PROP_WINDOW_LOCATION,
+		PROP_WINDOW_STATUS };
+    }
 
-	/* (non-Javadoc)
-	 * @see org.universAAL.middleware.owl.ManagedIndividual#isWellFormed()
-	 */
-	public boolean isWellFormed() {
-		return props.containsKey(PROP_HAS_TYPE)
-			&& props.containsKey(PROP_WINDOW_LOCATION)
-			&& props.contains(PROP_WINDOW_STATUS);
-	}
+    public static String getRDFSComment() {
+	return "The class of all windows.";
+    }
+
+    public static String getRDFSLabel() {
+	return "Window";
+    }
+
+    public Window() {
+	super();
+    }
+
+    public Window(String uri) {
+	super(uri);
+    }
+
+    public Window(String uri, AbsLocation loc) {
+	super(uri);
+	if (loc == null)
+	    throw new IllegalArgumentException();
+	props.put(PROP_WINDOW_LOCATION, loc);
+	props.put(PROP_WINDOW_STATUS, new Integer(0));
+    }
+
+    public Window(String uri, WindowType type, AbsLocation loc) {
+	super(uri);
+	if (type == null || loc == null)
+	    throw new IllegalArgumentException();
+
+	props.put(PROP_HAS_TYPE, type);
+	props.put(PROP_WINDOW_LOCATION, loc);
+    }
+
+    // public Location getAmbientCoverage() {
+    // return (Location) props.get(PROP_AMBIENT_COVERAGE);
+    // }
+
+    /*
+     * public int getBrightness() { Integer i = (Integer)
+     * props.get(PROP_SOCKET_VALUE); return (i == null)? -1 : i.intValue(); }
+     */
+
+    public WindowType getWindowType() {
+	return (WindowType) props.get(PROP_HAS_TYPE);
+    }
+
+    public AbsLocation getWindowLocation() {
+	return (AbsLocation) props.get(PROP_WINDOW_LOCATION);
+    }
+
+    // public void setAmbientCoverage(Location l) {
+    // if (l != null)
+    // props.put(PROP_AMBIENT_COVERAGE, l);
+    // }
+
+    // public void setBrightness(int percentage) {
+    // if (percentage > -1 && percentage < 101)
+    // props.put(PROP_SOCKET_VALUE, new Integer(percentage));
+    // }
+
+    public void setWindowLocation(AbsLocation l) {
+	if (l != null)
+	    props.put(PROP_WINDOW_LOCATION, l);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.universAAL.middleware.owl.ManagedIndividual#getPropSerializationType
+     * (java.lang.String)
+     */
+    public int getPropSerializationType(String propURI) {
+	return PROP_WINDOW_LOCATION.equals(propURI) ? PROP_SERIALIZATION_REDUCED
+		: PROP_SERIALIZATION_FULL;
+
+	// return (PROP_AMBIENT_COVERAGE.equals(propURI)
+	// || PROP_SOCKET_LOCATION.equals(propURI))?
+	// PROP_SERIALIZATION_REDUCED : PROP_SERIALIZATION_FULL;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.universAAL.middleware.owl.ManagedIndividual#isWellFormed()
+     */
+    public boolean isWellFormed() {
+	return props.containsKey(PROP_HAS_TYPE)
+		&& props.containsKey(PROP_WINDOW_LOCATION)
+		&& props.contains(PROP_WINDOW_STATUS);
+    }
 
 }
--- window/src/main/java/org/universAAL/ontology/window/WindowController.java
+++ window/src/main/java/org/universAAL/ontology/window/WindowController.java
@@ -16,7 +16,7 @@
 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 	See the License for the specific language governing permissions and
 	limitations under the License.
-*/
+ */
 
 package org.universAAL.ontology.window;
 
@@ -29,63 +29,68 @@ import org.universAAL.middleware.owl.Restriction;
  * 
  * @author Steeven Zeiss
  * @since 26.11.2009
- *
+ * 
  */
 public class WindowController extends Service {
-	public static final String MY_URI;
-	public static final String PROP_CONTROLS;
-	private static Hashtable windowServicesRestrictions = new Hashtable(1);
-	static {
-		MY_URI = Window.WINDOW_NAMESPACE + "WindowServices";
-		PROP_CONTROLS = Window.WINDOW_NAMESPACE + "controls";
-		register(WindowController.class);
-		addRestriction(
-				Restriction.getAllValuesRestriction(PROP_CONTROLS, Window.MY_URI),
-				new String[] {PROP_CONTROLS},
-				windowServicesRestrictions);
-	}
-	
-	public static Restriction getClassRestrictionsOnProperty(String propURI) {
-		if (propURI == null)
-			return null;
-		Object r = windowServicesRestrictions.get(propURI);
-		if (r instanceof Restriction)
-			return (Restriction) r;
-		return Service.getClassRestrictionsOnProperty(propURI);
-	}
-	
-	public static String getRDFSComment() {
-		return "The class of services controling windows.";
-	}
-	
-	public static String getRDFSLabel() {
-		return "WindowServices";
-	}
-	
-	public WindowController() {
-		super();
-	}
-	
-	public WindowController(String uri) {
-		super(uri);
-	}
+    public static final String MY_URI;
+    public static final String PROP_CONTROLS;
+    private static Hashtable windowServicesRestrictions = new Hashtable(1);
+    static {
+	MY_URI = Window.WINDOW_NAMESPACE + "WindowServices";
+	PROP_CONTROLS = Window.WINDOW_NAMESPACE + "controls";
+	register(WindowController.class);
+	addRestriction(Restriction.getAllValuesRestriction(PROP_CONTROLS,
+		Window.MY_URI), new String[] { PROP_CONTROLS },
+		windowServicesRestrictions);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.persona.ontology.Service#getClassLevelRestrictions()
-	 */
-	protected Hashtable getClassLevelRestrictions() {
-		return windowServicesRestrictions;
-	}
+    public static Restriction getClassRestrictionsOnProperty(String propURI) {
+	if (propURI == null)
+	    return null;
+	Object r = windowServicesRestrictions.get(propURI);
+	if (r instanceof Restriction)
+	    return (Restriction) r;
+	return Service.getClassRestrictionsOnProperty(propURI);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.universAAL.middleware.owl.ManagedIndividual#getPropSerializationType(java.lang.String)
-	 */
-	public int getPropSerializationType(String propURI) {
-		return PROP_CONTROLS.equals(propURI)? PROP_SERIALIZATION_FULL
-				: PROP_SERIALIZATION_OPTIONAL;
-	}
+    public static String getRDFSComment() {
+	return "The class of services controling windows.";
+    }
 
-	public boolean isWellFormed() {
-		return true;
-	}
+    public static String getRDFSLabel() {
+	return "WindowServices";
+    }
+
+    public WindowController() {
+	super();
+    }
+
+    public WindowController(String uri) {
+	super(uri);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.persona.ontology.Service#getClassLevelRestrictions()
+     */
+    protected Hashtable getClassLevelRestrictions() {
+	return windowServicesRestrictions;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.universAAL.middleware.owl.ManagedIndividual#getPropSerializationType
+     * (java.lang.String)
+     */
+    public int getPropSerializationType(String propURI) {
+	return PROP_CONTROLS.equals(propURI) ? PROP_SERIALIZATION_FULL
+		: PROP_SERIALIZATION_OPTIONAL;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
 }
