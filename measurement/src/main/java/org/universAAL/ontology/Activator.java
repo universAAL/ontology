6889bd74b55e4e69411088d949867a0cf04dd0ac
Alvaro Fides <alfiva@itaca.upv.es>
2011-03-21 09:33:33 +0000

    Formatted as Java conventions (ref.D2.3A)

--- measurement/src/main/java/org/universAAL/ontology/Activator.java
+++ measurement/src/main/java/org/universAAL/ontology/Activator.java
@@ -16,26 +16,26 @@
 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 	See the License for the specific language governing permissions and
 	limitations under the License.
-*/
+ */
 package org.universAAL.ontology;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-
 public class Activator implements BundleActivator {
 
-	static BundleContext context = null;
-	
-	public void start(BundleContext context) throws Exception {
-		Activator.context = context;
-		Class.forName("org.universAAL.ontology.measurement.DimensionMeasure");
-		Class.forName("org.universAAL.ontology.measurement.MultiDimensionMeasure");
-	}
-	
-	public void stop(BundleContext arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+    static BundleContext context = null;
+
+    public void start(BundleContext context) throws Exception {
+	Activator.context = context;
+	Class.forName("org.universAAL.ontology.measurement.DimensionMeasure");
+	Class
+		.forName("org.universAAL.ontology.measurement.MultiDimensionMeasure");
+    }
+
+    public void stop(BundleContext arg0) throws Exception {
+	// TODO Auto-generated method stub
+
+    }
 
 }
