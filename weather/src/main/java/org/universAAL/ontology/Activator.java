6365b144e02a03d218c0b0dd34c3bff9f1e4de8d
Alvaro Fides <alfiva@itaca.upv.es>
2011-03-21 09:43:10 +0000

    Formatted as Java conventions (ref.D2.3A)

--- weather/src/main/java/org/universAAL/ontology/Activator.java
+++ weather/src/main/java/org/universAAL/ontology/Activator.java
@@ -16,28 +16,27 @@
 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 	See the License for the specific language governing permissions and
 	limitations under the License.
-*/
+ */
 package org.universAAL.ontology;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-
 public class Activator implements BundleActivator {
 
-	static BundleContext context = null;
-	
-	public void start(BundleContext context) throws Exception {
-		Activator.context = context;
-		Class.forName("org.universAAL.ontology.weather.HeaterActuator");
-		Class.forName("org.universAAL.ontology.weather.RelHumiditySensor");
-		Class.forName("org.universAAL.ontology.weather.Temperature");
-		Class.forName("org.universAAL.ontology.weather.TempSensor");
-	}
-	
-	public void stop(BundleContext arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+    static BundleContext context = null;
+
+    public void start(BundleContext context) throws Exception {
+	Activator.context = context;
+	Class.forName("org.universAAL.ontology.weather.HeaterActuator");
+	Class.forName("org.universAAL.ontology.weather.RelHumiditySensor");
+	Class.forName("org.universAAL.ontology.weather.Temperature");
+	Class.forName("org.universAAL.ontology.weather.TempSensor");
+    }
+
+    public void stop(BundleContext arg0) throws Exception {
+	// TODO Auto-generated method stub
+
+    }
 
 }
--- weather/src/main/java/org/universAAL/ontology/weather/RelHumiditySensor.java
+++ weather/src/main/java/org/universAAL/ontology/weather/RelHumiditySensor.java
@@ -18,7 +18,7 @@
 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 	See the License for the specific language governing permissions and
 	limitations under the License.
-*/
+ */
 package org.universAAL.ontology.weather;
 
 import org.universAAL.middleware.rdf.TypeMapper;
@@ -27,49 +27,48 @@ import org.universAAL.ontology.phThing.Sensor;
 
 /**
  * @author <a href="mailto:alfiva@itaca.upv.es">Alvaro Fides Valero</a>
- *
+ * 
  */
-public class RelHumiditySensor extends Sensor{
-	public static final String MY_URI;
-	
-	static{
-		MY_URI=uAAL_DEVICE_NAMESPACE+"RelHumiditySensor";
-		register(RelHumiditySensor.class);
-	}
-	
-	public static Restriction getClassRestrictionsOnProperty(String propURI) {
-		if (PROP_MEASURED_VALUE.equals(propURI))
-			return Restriction.getAllValuesRestriction(propURI,
-					TypeMapper.getDatatypeURI(Integer.class));
-		return Sensor.getClassRestrictionsOnProperty(propURI);
-	}
-	
-	public RelHumiditySensor(){
-	}
-	
-	public RelHumiditySensor(String uri){
-		super(uri);
-	}
-	
-	public static String getRDFSComment() {
-		return "A Relative Humidity Sensor Device";
-	}
-	
-	public static String getRDFSLabel() {
-		return "Relative Humidity Sensor";
-	}
+public class RelHumiditySensor extends Sensor {
+    public static final String MY_URI;
 
-	public boolean isWellFormed() {
-		return true;
-	}
+    static {
+	MY_URI = uAAL_DEVICE_NAMESPACE + "RelHumiditySensor";
+	register(RelHumiditySensor.class);
+    }
 
-	public int getMeasuredvalue(){
-		return ((Integer)props.get(PROP_MEASURED_VALUE)).intValue();
-	}
-	
-	public void setMeasuredValue(int value){
-		props.put(PROP_MEASURED_VALUE, new Integer(value));
-	}
+    public static Restriction getClassRestrictionsOnProperty(String propURI) {
+	if (PROP_MEASURED_VALUE.equals(propURI))
+	    return Restriction.getAllValuesRestriction(propURI, TypeMapper
+		    .getDatatypeURI(Integer.class));
+	return Sensor.getClassRestrictionsOnProperty(propURI);
+    }
 
+    public RelHumiditySensor() {
+    }
+
+    public RelHumiditySensor(String uri) {
+	super(uri);
+    }
+
+    public static String getRDFSComment() {
+	return "A Relative Humidity Sensor Device";
+    }
+
+    public static String getRDFSLabel() {
+	return "Relative Humidity Sensor";
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+
+    public int getMeasuredvalue() {
+	return ((Integer) props.get(PROP_MEASURED_VALUE)).intValue();
+    }
+
+    public void setMeasuredValue(int value) {
+	props.put(PROP_MEASURED_VALUE, new Integer(value));
+    }
 
 }
