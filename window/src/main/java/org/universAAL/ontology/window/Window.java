9a0cef5556414dd42e64617467480345dbbd52be
Alvaro Fides <alfiva@itaca.upv.es>
2011-05-13 11:30:03 +0000

    Added javadoc to code

--- window/src/main/java/org/universAAL/ontology/window/Window.java
+++ window/src/main/java/org/universAAL/ontology/window/Window.java
@@ -28,6 +28,10 @@ import org.universAAL.middleware.owl.Restriction;
 import org.universAAL.ontology.phThing.Device;
 
 /**
+ * Ontological representation of a window as a device. Methods included in this
+ * class are the mandatory ones for representing an ontological concept in Java
+ * classes for uAAL. Usually it includes getters and setters for most of its
+ * properties.
  * 
  * @author Steeven Zeiss
  * 
