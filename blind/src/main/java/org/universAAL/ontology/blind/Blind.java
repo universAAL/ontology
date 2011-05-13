01d20f0db888e40d79c5dc8ecc92d3052741f053
Alvaro Fides <alfiva@itaca.upv.es>
2011-05-13 11:32:24 +0000

    Added javadoc to code

--- blind/src/main/java/org/universAAL/ontology/blind/Blind.java
+++ blind/src/main/java/org/universAAL/ontology/blind/Blind.java
@@ -27,6 +27,10 @@ import org.universAAL.middleware.owl.Restriction;
 import org.universAAL.ontology.phThing.Device;
 
 /**
+ * Ontological representation of a blind as a device. Methods included in this
+ * class are the mandatory ones for representing an ontological concept in Java
+ * classes for uAAL. Usually it includes getters and setters for most of its
+ * properties.
  * 
  * @author Steeven Zeiss
  * @since 26.11.2009
