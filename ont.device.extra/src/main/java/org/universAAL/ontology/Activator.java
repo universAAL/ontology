21a8bfbc5073f88e5293154a8ba78e2e9761d39b
Alvaro Fides <alfiva@itaca.upv.es>
2011-05-26 12:07:20 +0000

    Removed all concepts that have been moved to the other new ontologies

--- ont.device.extra/src/main/java/org/universAAL/ontology/Activator.java
+++ ont.device.extra/src/main/java/org/universAAL/ontology/Activator.java
@@ -3,7 +3,6 @@ package org.universAAL.ontology;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.universAAL.ontology.device.home.*;
-import org.universAAL.ontology.device.wearable.*;
 
 public class Activator implements BundleActivator {
 
@@ -13,22 +12,11 @@ public class Activator implements BundleActivator {
 	Activator.context = context;
 	Class.forName(CarpetSensor.class.getName());
 	Class.forName(ContactSensor.class.getName());
-	Class.forName(CurtainActuator.class.getName());
-	Class.forName(DimmerActuator.class.getName());
-	Class.forName(FaucetSensor.class.getName());
-	Class.forName(FireDetector.class.getName());
-	Class.forName(GasDetector.class.getName());
-	Class.forName(GlassBreakSensor.class.getName());
 	Class.forName(LoudSpeaker.class.getName());
 	Class.forName(Oven.class.getName());
 	Class.forName(PresenceDetector.class.getName());
 	Class.forName(SirenActuator.class.getName());
-	Class.forName(Stereoset.class.getName());
 	Class.forName(Strap.class.getName());
-	Class.forName(TV.class.getName());
-	Class.forName(WaterActuator.class.getName());
-	Class.forName(FallDetector.class.getName());
-	Class.forName(PanicButton.class.getName());
     }
 
     public void stop(BundleContext arg0) throws Exception {
