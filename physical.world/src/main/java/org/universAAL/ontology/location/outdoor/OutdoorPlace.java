a953b8765892b1bcb7d9297b096a55dd13af9b56
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2011-12-21 04:31:07 +0000

    preparing release

---
+++ physical.world/src/main/java/org/universAAL/ontology/location/outdoor/OutdoorPlace.java
@@ -0,0 +1,96 @@
+/*
+	Copyright 2008-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer Gesellschaft - Institut fuer Graphische Datenverarbeitung 
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ontology.location.outdoor;
+
+import org.universAAL.ontology.location.Place;
+import org.universAAL.ontology.shape.Shape;
+
+/**
+ * Ontological representation of a place located outdoors. Methods included in
+ * this class are the mandatory ones for representing an ontological concept in
+ * Java classes for uAAL. Usually it includes getters and setters for most of
+ * its properties.
+ * 
+ * @author chwirth
+ * 
+ */
+public class OutdoorPlace extends Place {
+
+    public static final String MY_URI;
+
+    static {
+	MY_URI = uAAL_LOCATION_NAMESPACE + "OutdoorPlace";
+	register(OutdoorPlace.class);
+    }
+
+    /**
+     * Creates a Place object
+     * 
+     * @param uri
+     *            the object URI
+     */
+    public OutdoorPlace(String uri) {
+	super(uri);
+    }
+
+    /**
+     * Creates a Place object
+     */
+    public OutdoorPlace() {
+	super();
+    }
+
+    public OutdoorPlace(String uri, String name, Shape shape) {
+	super(uri, name, shape);
+    }
+
+    public OutdoorPlace(String uri, Shape shape) {
+	super(uri, shape);
+    }
+
+    /**
+     * Creates a OutdoorPlace object
+     * 
+     * @param uri
+     *            this value can also be a null object
+     * @param name
+     *            The place name. A null object is not allowed
+     */
+    public OutdoorPlace(String uri, String name) {
+	super(uri, name);
+    }
+
+    /**
+     * Returns a human readable description on the essence of this ontology
+     * class.
+     */
+    public static String getRDFSComment() {
+	return "The root class for all outdoor places.";
+    }
+
+    /**
+     * Returns a label with which this ontology class can be introduced to human
+     * users.
+     */
+    public static String getRDFSLabel() {
+	return "OutdoorPlace";
+    }
+
+}
---
+++ physical.world/src/main/java/org/universAAL/ontology/phThing/DeviceService.java
@@ -0,0 +1,88 @@
+/*
+ Copyright 2008-2011 ITACA-TSB, http://www.tsb.upv.es
+ Instituto Tecnologico de Aplicaciones de Comunicacion 
+ Avanzadas - Grupo Tecnologias para la Salud y el 
+ Bienestar (TSB)
+
+ See the NOTICE file distributed with this work for additional 
+ information regarding copyright ownership
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package org.universAAL.ontology.phThing;
+
+import java.util.Hashtable;
+
+import org.universAAL.middleware.owl.Restriction;
+import org.universAAL.middleware.service.owl.Service;
+import org.universAAL.ontology.phThing.Device;
+
+/**
+ * Ontological service that controls any HW device. Methods
+ * included in this class are the mandatory ones for representing an ontological
+ * service in Java classes for uAAL.
+ * 
+ * @author alfiva
+ */
+public class DeviceService extends Service {
+    public static final String MY_URI;
+    public static final String PROP_CONTROLS;
+    private static Hashtable serviceRestrictions = new Hashtable(1);
+    static {
+	MY_URI = Device.uAAL_DEVICE_NAMESPACE + "DeviceService";
+	PROP_CONTROLS = Device.uAAL_DEVICE_NAMESPACE + "controls";
+	register(DeviceService.class);
+	addRestriction(Restriction.getAllValuesRestriction(PROP_CONTROLS,
+		Device.MY_URI), new String[] { PROP_CONTROLS },
+		serviceRestrictions);
+    }
+
+    public static Restriction getClassRestrictionsOnProperty(String propURI) {
+	if (propURI == null)
+	    return null;
+	Object r = serviceRestrictions.get(propURI);
+	if (r instanceof Restriction)
+	    return (Restriction) r;
+	return Service.getClassRestrictionsOnProperty(propURI);
+    }
+
+    public static String getRDFSComment() {
+	return "The class of services controling devices.";
+    }
+
+    public static String getRDFSLabel() {
+	return "DeviceService";
+    }
+
+    public DeviceService(String uri) {
+	super(uri);
+    }
+    
+    public DeviceService() {
+	super();
+    }
+
+    protected Hashtable getClassLevelRestrictions() {
+	return serviceRestrictions;
+    }
+
+    public int getPropSerializationType(String propURI) {
+	return PROP_CONTROLS.equals(propURI) ? PROP_SERIALIZATION_FULL
+		: PROP_SERIALIZATION_OPTIONAL;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+}
