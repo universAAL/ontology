65bcd810cd078ef6eff10d70768245900c608d0e
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2011-12-21 04:24:49 +0000

    preparing release

---
+++ furniture/src/main/java/org/universAAL/ontology/furniture/Furniture.java
@@ -0,0 +1,118 @@
+/*
+	Copyright 2008-20104 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer Gesellschaft - Institut fuer Gtaphische Datenverarbeitung 
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ontology.furniture;
+
+import org.universAAL.middleware.owl.Restriction;
+
+import org.universAAL.ontology.phThing.PhysicalThing;
+import org.universAAL.ontology.shape.Shape;
+
+/**
+ * Ontological representation of any furniture. Methods included in this class
+ * are the mandatory ones for representing an ontological concept in Java
+ * classes for uAAL. Usually it includes getters and setters for most of its
+ * properties.
+ * 
+ */
+public class Furniture extends PhysicalThing {
+    public static final String MY_URI;
+    public static final String FURNITURE_NAMESPACE = uAAL_NAMESPACE_PREFIX
+	    + "Furniture.owl#";
+    public static final String PROP_FURNITURE_TYPE;
+
+    static {
+	MY_URI = FURNITURE_NAMESPACE + "Furniture";
+	PROP_FURNITURE_TYPE = FURNITURE_NAMESPACE + "FurnitureType";
+	register(Furniture.class);
+    }
+
+    public static Restriction getClassRestrictionsOnProperty(String propURI) {
+	if (PROP_FURNITURE_TYPE.equals(propURI))
+	    return Restriction.getAllValuesRestrictionWithCardinality(propURI,
+		    FurnitureType.MY_URI, 1, 0);
+	return PhysicalThing.getClassRestrictionsOnProperty(propURI);
+    }
+
+    public static String[] getStandardPropertyURIs() {
+	String[] inherited = PhysicalThing.getStandardPropertyURIs();
+	String[] toReturn = new String[inherited.length + 1];
+	int i = 0;
+	while (i < inherited.length) {
+	    toReturn[i] = inherited[i];
+	    i++;
+	}
+	toReturn[i] = PROP_FURNITURE_TYPE;
+	return toReturn;
+    }
+
+    public Furniture() {
+    }
+
+    public Furniture(String uri) {
+	super(uri);
+    }
+
+    public Furniture(String uri, FurnitureType type) {
+	super(uri);
+	setFurnitureType(type);
+    }
+
+    public Furniture(String uri, FurnitureType type, Shape shape) {
+	super(uri);
+	setShape(shape);
+	setFurnitureType(type);
+    }
+
+    public static String getRDFSComment() {
+	return "A furniture";
+    }
+
+    public static String getRDFSLabel() {
+	return "Furniture";
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+
+    public FurnitureType getFurnitureType() {
+	return (FurnitureType) props.get(PROP_FURNITURE_TYPE);
+    }
+
+    public void setFurnitureType(FurnitureType function) {
+	if (function == null)
+	    throw new IllegalArgumentException();
+	props.put(PROP_FURNITURE_TYPE, function);
+    }
+
+    public void clearFurnitureType() {
+	props.put(PROP_FURNITURE_TYPE, null);
+    }
+
+    public int getPropSerializationType(String propURI) {
+	if (super.getPropSerializationType(propURI) != PROP_SERIALIZATION_OPTIONAL)
+	    return super.getPropSerializationType(propURI);
+	if (PROP_FURNITURE_TYPE.equals(propURI))
+	    return PROP_SERIALIZATION_REDUCED;
+
+	return PROP_SERIALIZATION_OPTIONAL;
+    }
+
+}
---
+++ furniture/src/main/java/org/universAAL/ontology/furniture/FurnitureType.java
@@ -0,0 +1,137 @@
+/*
+	Copyright 2008-2010 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute of Computer Graphics Research 
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ontology.furniture;
+
+import org.universAAL.middleware.owl.ManagedIndividual;
+
+/**
+ * Ontological enumeration of possible furniture types. Methods included in this
+ * class are the mandatory ones for representing an ontological enumeration in
+ * Java classes for uAAL.
+ * 
+ */
+public class FurnitureType extends ManagedIndividual {
+
+    public static final String MY_URI;
+
+    public static final int CHAIR = 0;
+    public static final int TABLE = 1;
+    public static final int SHELF = 2;
+    public static final int LOCKER = 3;
+    public static final int SOFA = 4;
+    public static final int BED = 5;
+    public static final int OTHER = 6;
+
+    private int order;
+
+    private static final String[] names = { "Chair", "Table", "Shelf",
+	    "Locker", "Sofa", "Bed", "Other" };
+
+    static {
+	MY_URI = Furniture.FURNITURE_NAMESPACE + "FurnitureType";
+	register(FurnitureType.class);
+    }
+
+    public static final FurnitureType Chair = new FurnitureType(CHAIR);
+    public static final FurnitureType Table = new FurnitureType(TABLE);
+    public static final FurnitureType Shelf = new FurnitureType(SHELF);
+    public static final FurnitureType Locker = new FurnitureType(LOCKER);
+    public static final FurnitureType Sofa = new FurnitureType(SOFA);
+    public static final FurnitureType Bed = new FurnitureType(BED);
+    public static final FurnitureType Other = new FurnitureType(OTHER);
+
+    /**
+     * Returns the list of all class members guaranteeing that no other members
+     * will be created after a call to this method.
+     */
+    public static ManagedIndividual[] getEnumerationMembers() {
+	return new ManagedIndividual[] { Chair, Table, Shelf, Locker, Sofa,
+		Bed, Other };
+    }
+
+    /**
+     * Returns the modality with the given URI.
+     */
+    public static ManagedIndividual getIndividualByURI(String instanceURI) {
+	return (instanceURI != null && instanceURI
+		.startsWith(Furniture.FURNITURE_NAMESPACE)) ? valueOf(instanceURI
+		.substring(Furniture.FURNITURE_NAMESPACE.length()))
+		: null;
+    }
+
+    public static final FurnitureType valueOf(String name) {
+	for (int i = 0; i <= 10; i++)
+	    if (names[i].equals(name))
+		return getLevelByOrder(i);
+	return null;
+    }
+
+    public static FurnitureType getLevelByOrder(int order) {
+	return (FurnitureType) getEnumerationMembers()[order];
+    }
+
+    /**
+     * Creates a RoomFunction object
+     */
+    public FurnitureType() {
+	super();
+    }
+
+    /**
+     * Creates a RoomFunction object
+     * 
+     * @param uri
+     *            this value can also be a null object
+     * @param name
+     *            The function number. A null object is not allowed
+     */
+    public FurnitureType(int Function) {
+	super();
+	this.order = Function;
+    }
+
+    public String name() {
+	return names[order];
+    }
+
+    public int ord() {
+	return order;
+    }
+
+    /**
+     * Returns a human readable description on the essence of this ontology
+     * class.
+     */
+    public static String getRDFSComment() {
+	return "Describes the type of a furniture.";
+    }
+
+    /**
+     * Returns a label with which this ontology class can be introduced to human
+     * users.
+     */
+    public static String getRDFSLabel() {
+	return "Furniture Type";
+    }
+
+    public int getPropSerializationType(String arg0) {
+	return PROP_SERIALIZATION_OPTIONAL;
+    }
+}
