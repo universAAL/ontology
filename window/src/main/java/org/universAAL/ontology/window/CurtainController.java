4ebf012b3632ac7526fa37c8ad1b2d077264c825
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2011-12-21 04:35:33 +0000

    preparing release

---
+++ window/src/main/java/org/universAAL/ontology/window/CurtainController.java
@@ -0,0 +1,106 @@
+/*
+	Copyright 2008-2010 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute of Computer Graphics Research 
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+
+package org.universAAL.ontology.window;
+
+import java.util.Hashtable;
+
+import org.universAAL.middleware.service.owl.Service;
+import org.universAAL.middleware.owl.Restriction;
+import org.universAAL.ontology.phThing.DeviceService;
+
+/**
+ * Ontological service that controls a blind. Methods included in this class are
+ * the mandatory ones for representing an ontological service in Java classes
+ * for uAAL.
+ * 
+ * @author Steeven Zeiss
+ * @since 26.11.2009
+ * 
+ */
+public class CurtainController extends DeviceService {
+    public static final String MY_URI;
+    public static final String PROP_CONTROLS;
+
+    private static Hashtable blindServicesRestrictions = new Hashtable(1);
+    static {
+	MY_URI = WindowActuator.WINDOW_NAMESPACE + "CurtainController";
+	PROP_CONTROLS = WindowActuator.WINDOW_NAMESPACE + "controls";
+	// PROP_STATUS = Blind.BLIND_NAMESPACE+ "status";
+	register(CurtainController.class);
+	addRestriction(Restriction.getAllValuesRestriction(PROP_CONTROLS,
+		BlindActuator.MY_URI), new String[] { PROP_CONTROLS },
+		blindServicesRestrictions);
+	// addRestriction(
+	// Restriction.getAllValuesRestriction(PROP_STATUS, Blind.MY_URI),
+	// new String[]{PROP_STATUS},
+	// blindServicesRestrictions);
+    }
+
+    public static Restriction getClassRestrictionsOnProperty(String propURI) {
+	if (propURI == null)
+	    return null;
+	Object r = blindServicesRestrictions.get(propURI);
+	if (r instanceof Restriction)
+	    return (Restriction) r;
+	return Service.getClassRestrictionsOnProperty(propURI);
+    }
+
+    public static String getRDFSComment() {
+	return "The class of services controling blinds.";
+    }
+
+    public static String getRDFSLabel() {
+	return "BlindsServices";
+    }
+
+    public CurtainController() {
+	super();
+    }
+
+    public CurtainController(String uri) {
+	super(uri);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.persona.ontology.Service#getClassLevelRestrictions()
+     */
+    protected Hashtable getClassLevelRestrictions() {
+	return blindServicesRestrictions;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.persona.ontology.ManagedIndividual#getPropSerializationType(java.
+     * lang.String)
+     */
+    public int getPropSerializationType(String propURI) {
+	return PROP_CONTROLS.equals(propURI) ? PROP_SERIALIZATION_FULL
+		: PROP_SERIALIZATION_OPTIONAL;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+}
