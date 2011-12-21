8c2bd5c636d2d6460dbef337e01285abe989de40
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2011-12-21 04:31:07 +0000

    preparing release

---
+++ physical.world/src/main/java/org/universAAL/ontology/location/address/MailBox.java
@@ -0,0 +1,127 @@
+package org.universAAL.ontology.location.address;
+
+import org.universAAL.middleware.owl.Restriction;
+import org.universAAL.middleware.rdf.TypeMapper;
+import org.universAAL.ontology.location.Location;
+
+/**
+ * Ontological representation of a Mail Box. It is used when the address
+ * represents an entity that has no physical representation and/or only has a
+ * mail box in a post office, not corresponding to its actual location. Methods
+ * included in this class are the mandatory ones for representing an ontological
+ * concept in Java classes for uAAL. Usually it includes getters and setters for
+ * most of its properties.
+ * 
+ * @author alfiva
+ * 
+ */
+public class MailBox extends Address {
+    public static final String MY_URI;
+    public static final String PROP_POST_OFFICE_BOX;
+
+    static {
+	MY_URI = Location.uAAL_LOCATION_NAMESPACE + "MailBox";
+
+	PROP_POST_OFFICE_BOX = Location.uAAL_LOCATION_NAMESPACE
+		+ "postOfficeBox";
+
+	register(MailBox.class);
+    }
+
+    public static Restriction getClassRestrictionsOnProperty(String propURI) {
+	if (PROP_POST_OFFICE_BOX.equals(propURI))
+	    return Restriction.getAllValuesRestrictionWithCardinality(propURI,
+		    TypeMapper.getDatatypeURI(String.class), 1, 1);
+
+	return Address.getClassRestrictionsOnProperty(propURI);
+    }
+
+    public static String[] getStandardPropertyURIs() {
+	String[] inherited = Address.getStandardPropertyURIs();
+	String[] toReturn = new String[inherited.length + 1];
+
+	int i = 0;
+	while (i < inherited.length) {
+	    toReturn[i] = inherited[i];
+	    i++;
+	}
+	toReturn[i] = PROP_POST_OFFICE_BOX;
+	return toReturn;
+    }
+
+    /**
+     * Creates a MailBox object
+     */
+    public MailBox() {
+
+    }
+
+    /**
+     * Creates a MailBox object
+     * 
+     * @param uri
+     *            the object URI
+     */
+    public MailBox(String uri) {
+	super(uri);
+    }
+
+    public MailBox(String uri, String postOffciceBox) {
+	super(uri);
+	props.put(PROP_POST_OFFICE_BOX, postOffciceBox);
+    }
+
+    /**
+     * Returns a human readable description on the essence of this ontology
+     * class.
+     */
+    public static String getRDFSComment() {
+	return "A mail box in a post office";
+    }
+
+    /**
+     * Returns a label with which this ontology class can be introduced to human
+     * users.
+     */
+    public static String getRDFSLabel() {
+	return "Mail Box";
+    }
+
+    public int getPropSerializationType(String propURI) {
+	return PROP_SERIALIZATION_FULL;
+    }
+
+    public boolean isWellFormed() {
+	return props.contains(PROP_POST_OFFICE_BOX);
+    }
+
+    /**
+     * Gets the post office box value of the Address
+     * 
+     * @return The post office box value
+     */
+    public String getPostOfficeBox() {
+	return (String) props.get(PROP_POST_OFFICE_BOX);
+    }
+
+    /**
+     * Sets the post office box value of the Address
+     * 
+     * @param reg
+     *            The value to set
+     */
+    public void setPostOfficeBox(String val) {
+	props.put(PROP_POST_OFFICE_BOX, val);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.universAAL.ontology.location.address.Address#toReadableString()
+     */
+    public String toReadableString() {
+	String parent = super.toReadableString();
+	return parent + (parent.isEmpty() ? "\n" : "")
+		+ this.getPostOfficeBox();
+    }
+}
---
+++ physical.world/src/main/java/org/universAAL/ontology/phThing/Sensor.java
@@ -0,0 +1,87 @@
+/*
+	Copyright 2008-2014 ITACA-TSB, http://www.tsb.upv.es
+	Instituto Tecnologico de Aplicaciones de Comunicacion 
+	Avanzadas - Grupo Tecnologias para la Salud y el 
+	Bienestar (TSB)
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ontology.phThing;
+
+import org.universAAL.middleware.owl.Restriction;
+
+/**
+ * Ontological representation of a sensor: a device that can measure values from
+ * the physical world. Methods included in this class are the mandatory ones for
+ * representing an ontological concept in Java classes for uAAL. Usually it
+ * includes getters and setters for most of its properties.
+ * 
+ * @author <a href="mailto:alfiva@itaca.upv.es">Alvaro Fides Valero</a>
+ * 
+ */
+public class Sensor extends Device {
+
+    public static final String MY_URI;
+    public static final String PROP_MEASURED_VALUE;
+
+    static {
+	MY_URI = Device.uAAL_DEVICE_NAMESPACE + "Sensor";
+	PROP_MEASURED_VALUE = Device.uAAL_DEVICE_NAMESPACE + "measuredValue";
+	register(Sensor.class);
+    }
+
+    public static Restriction getClassRestrictionsOnProperty(String propURI) {
+	if (PROP_MEASURED_VALUE.equals(propURI))
+	    return Restriction.getCardinalityRestriction(propURI, 1, 1);
+	return Device.getClassRestrictionsOnProperty(propURI);
+    }
+
+    public static String[] getStandardPropertyURIs() {
+	String[] inherited = Device.getStandardPropertyURIs();
+	String[] toReturn = new String[inherited.length + 1];
+	int i = 0;
+	while (i < inherited.length) {
+	    toReturn[i] = inherited[i];
+	    i++;
+	}
+	toReturn[i] = PROP_MEASURED_VALUE;
+	return toReturn;
+    }
+
+    public Sensor() {
+    }
+
+    public Sensor(String uri) {
+	super(uri);
+    }
+
+    public static String getRDFSComment() {
+	return "A Sensor Device";
+    }
+
+    public static String getRDFSLabel() {
+	return "Sensor";
+    }
+
+    public int getPropSerializationType(String propURI) {
+	return PROP_SERIALIZATION_FULL;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+
+}
