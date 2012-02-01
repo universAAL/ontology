6d0598a4c8add3b04ca8b5e00ea86d96f56d31c8
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-02-01 13:26:32 +0000

    Merge into trunk

---
+++ profile.health/src/test/java/org/universAAL/ontology/profile/health/ArtifactIntegrationTest.java
@@ -0,0 +1,18 @@
+package src.test.java.org.universAAL.ontology.profile.health;
+
+import org.universAAL.itests.IntegrationTest;
+
+/**
+ * Here developer's of this artifact should code their integration tests.
+ * 
+ * @author rotgier
+ * 
+ */
+public class ArtifactIntegrationTest extends IntegrationTest {
+
+
+    public void testComposite() {
+	logAllBundles();
+    }
+
+}
