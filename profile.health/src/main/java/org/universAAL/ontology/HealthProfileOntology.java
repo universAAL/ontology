c75f05f32537b61c6a72ec7990db5d4c7f80166a
Alvaro Fides <alfiva@itaca.upv.es>
2012-03-05 15:47:22 +0000

    Corrected extension to AP instead of User

--- profile.health/src/main/java/org/universAAL/ontology/HealthProfileOntology.java
+++ profile.health/src/main/java/org/universAAL/ontology/HealthProfileOntology.java
@@ -18,7 +18,6 @@
  */
 package org.universAAL.ontology;
 
-
 import org.universAAL.middleware.owl.DataRepOntology;
 import org.universAAL.middleware.owl.ManagedIndividual;
 import org.universAAL.middleware.owl.MergedRestriction;
@@ -28,8 +27,8 @@ import org.universAAL.middleware.rdf.Resource;
 import org.universAAL.middleware.rdf.TypeMapper;
 import org.universAAL.middleware.ui.owl.AccessImpairment;
 import org.universAAL.middleware.ui.owl.UIBusOntology;
+import org.universAAL.ontology.profile.AssistedPersonProfile;
 import org.universAAL.ontology.profile.SubProfile;
-import org.universAAL.ontology.profile.UserProfile;
 import org.universAAL.ontology.profile.health.HealthProfile;
 import org.universAAL.ontology.profile.health.Illness;
 import org.universAAL.ontology.profile.health.Medicine;
@@ -46,8 +45,9 @@ public final class HealthProfileOntology extends Ontology {
 
     public static final String NAMESPACE = Resource.uAAL_NAMESPACE_PREFIX
 	    + "Profile.owl#";
-    
-    public static final String PROP_HEALTH_PROFILE = UserProfile.PROP_HAS_SUB_PROFILE+"Health";
+
+    public static final String PROP_HEALTH_PROFILE = AssistedPersonProfile.PROP_HAS_SUB_PROFILE
+	    + "Health";
 
     public HealthProfileOntology() {
 	super(NAMESPACE);
@@ -55,15 +55,13 @@ public final class HealthProfileOntology extends Ontology {
 
     public void create() {
 	Resource r = getInfo();
-	r
-		.setResourceComment("The ontology defining a health profile.");
+	r.setResourceComment("The ontology defining a health profile.");
 	r.setResourceLabel("HealthProfile");
 	addImport(DataRepOntology.NAMESPACE);
 	addImport(ProfileOntology.NAMESPACE);
 	addImport(UIBusOntology.NAMESPACE);
 
 	OntClassInfoSetup oci;
-
 
 	// load Illness
 	oci = createNewOntClassInfo(Illness.MY_URI, factory, 1);
@@ -85,18 +83,18 @@ public final class HealthProfileOntology extends Ontology {
 	oci.addDatatypeProperty(Illness.PROP_S_CHOLESTEROL).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			Illness.PROP_S_CHOLESTEROL, TypeMapper
-				.getDatatypeURI(Boolean.class), 1, 1));
+			Illness.PROP_S_CHOLESTEROL,
+			TypeMapper.getDatatypeURI(Boolean.class), 1, 1));
 	oci.addDatatypeProperty(Illness.PROP_S_HYPERTENSION).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			Illness.PROP_S_HYPERTENSION, TypeMapper
-				.getDatatypeURI(Boolean.class), 1, 1));
+			Illness.PROP_S_HYPERTENSION,
+			TypeMapper.getDatatypeURI(Boolean.class), 1, 1));
 	oci.addDatatypeProperty(Illness.PROP_S_DIGESTION_PROB).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			Illness.PROP_S_DIGESTION_PROB, TypeMapper
-				.getDatatypeURI(Boolean.class), 1, 1));
+			Illness.PROP_S_DIGESTION_PROB,
+			TypeMapper.getDatatypeURI(Boolean.class), 1, 1));
 	oci.addDatatypeProperty(Illness.PROP_S_ALLERGY).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(Illness.PROP_S_ALLERGY,
@@ -104,45 +102,45 @@ public final class HealthProfileOntology extends Ontology {
 	oci.addDatatypeProperty(Illness.PROP_S_ARTERIOPATHY).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			Illness.PROP_S_ARTERIOPATHY, TypeMapper
-				.getDatatypeURI(Boolean.class), 1, 1));
+			Illness.PROP_S_ARTERIOPATHY,
+			TypeMapper.getDatatypeURI(Boolean.class), 1, 1));
 	oci.addDatatypeProperty(Illness.PROP_S_DIABETES).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			Illness.PROP_S_DIABETES, TypeMapper
-				.getDatatypeURI(Boolean.class), 1, 1));
+			Illness.PROP_S_DIABETES,
+			TypeMapper.getDatatypeURI(Boolean.class), 1, 1));
 	oci.addDatatypeProperty(Illness.PROP_S_ISCHEMIC_CARDIOPATHY)
 		.setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			Illness.PROP_S_ISCHEMIC_CARDIOPATHY, TypeMapper
-				.getDatatypeURI(Boolean.class), 1, 1));
+			Illness.PROP_S_ISCHEMIC_CARDIOPATHY,
+			TypeMapper.getDatatypeURI(Boolean.class), 1, 1));
 	oci.addDatatypeProperty(Illness.PROP_S_DEMENTIA).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			Illness.PROP_S_DEMENTIA, TypeMapper
-				.getDatatypeURI(Boolean.class), 1, 1));
+			Illness.PROP_S_DEMENTIA,
+			TypeMapper.getDatatypeURI(Boolean.class), 1, 1));
 	oci.addDatatypeProperty(Illness.PROP_S_MALNUTRITION).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			Illness.PROP_S_MALNUTRITION, TypeMapper
-				.getDatatypeURI(Boolean.class), 1, 1));
+			Illness.PROP_S_MALNUTRITION,
+			TypeMapper.getDatatypeURI(Boolean.class), 1, 1));
 	oci.addDatatypeProperty(Illness.PROP_S_OSTEOPOROSIS).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			Illness.PROP_S_OSTEOPOROSIS, TypeMapper
-				.getDatatypeURI(Boolean.class), 1, 1));
+			Illness.PROP_S_OSTEOPOROSIS,
+			TypeMapper.getDatatypeURI(Boolean.class), 1, 1));
 	oci.addDatatypeProperty(Illness.PROP_S_OVERWEIGHT).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			Illness.PROP_S_OVERWEIGHT, TypeMapper
-				.getDatatypeURI(Boolean.class), 1, 1));
+			Illness.PROP_S_OVERWEIGHT,
+			TypeMapper.getDatatypeURI(Boolean.class), 1, 1));
 	oci.addDatatypeProperty(Illness.PROP_S_METABOLIC_SYNDROME)
 		.setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			Illness.PROP_S_METABOLIC_SYNDROME, TypeMapper
-				.getDatatypeURI(Boolean.class), 1, 1));
+			Illness.PROP_S_METABOLIC_SYNDROME,
+			TypeMapper.getDatatypeURI(Boolean.class), 1, 1));
 
 	// load Medicine
 	oci = createNewOntClassInfo(Medicine.MY_URI, factory, 2);
@@ -156,8 +154,8 @@ public final class HealthProfileOntology extends Ontology {
 	oci.addDatatypeProperty(Medicine.PROP_S_QUANTITY).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			Medicine.PROP_S_QUANTITY, TypeMapper
-				.getDatatypeURI(Integer.class), 1, 1));
+			Medicine.PROP_S_QUANTITY,
+			TypeMapper.getDatatypeURI(Integer.class), 1, 1));
 
 	// load HealthProfile
 	oci = createNewOntClassInfo(HealthProfile.MY_URI, factory, 0);
@@ -167,58 +165,58 @@ public final class HealthProfileOntology extends Ontology {
 	oci.addDatatypeProperty(HealthProfile.PROP_S_AGE).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			HealthProfile.PROP_S_AGE, TypeMapper
-				.getDatatypeURI(Integer.class), 1, 1));
+			HealthProfile.PROP_S_AGE,
+			TypeMapper.getDatatypeURI(Integer.class), 1, 1));
 	oci.addDatatypeProperty(HealthProfile.PROP_S_DOCTOR_REFERENCE)
 		.setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			HealthProfile.PROP_S_DOCTOR_REFERENCE, TypeMapper
-				.getDatatypeURI(Integer.class), 1, 1));
+			HealthProfile.PROP_S_DOCTOR_REFERENCE,
+			TypeMapper.getDatatypeURI(Integer.class), 1, 1));
 	oci.addDatatypeProperty(HealthProfile.PROP_S_NUTRITIONAL_REFERENCE)
 		.setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			HealthProfile.PROP_S_NUTRITIONAL_REFERENCE, TypeMapper
-				.getDatatypeURI(Integer.class), 1, 1));
+			HealthProfile.PROP_S_NUTRITIONAL_REFERENCE,
+			TypeMapper.getDatatypeURI(Integer.class), 1, 1));
 	oci.addDatatypeProperty(HealthProfile.PROP_S_SOCIAL_CARE_CENTER)
 		.setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			HealthProfile.PROP_S_SOCIAL_CARE_CENTER, TypeMapper
-				.getDatatypeURI(Integer.class), 1, 1));
+			HealthProfile.PROP_S_SOCIAL_CARE_CENTER,
+			TypeMapper.getDatatypeURI(Integer.class), 1, 1));
 	oci.addDatatypeProperty(HealthProfile.PROP_S_HOSPITAL).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			HealthProfile.PROP_S_HOSPITAL, TypeMapper
-				.getDatatypeURI(Integer.class), 1, 1));
+			HealthProfile.PROP_S_HOSPITAL,
+			TypeMapper.getDatatypeURI(Integer.class), 1, 1));
 	oci.addDatatypeProperty(HealthProfile.PROP_S_HEIGHT).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			HealthProfile.PROP_S_HEIGHT, TypeMapper
-				.getDatatypeURI(Integer.class), 1, 1));
+			HealthProfile.PROP_S_HEIGHT,
+			TypeMapper.getDatatypeURI(Integer.class), 1, 1));
 	oci.addDatatypeProperty(HealthProfile.PROP_S_ALCOHOL).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			HealthProfile.PROP_S_ALCOHOL, TypeMapper
-				.getDatatypeURI(Integer.class), 1, 1));
+			HealthProfile.PROP_S_ALCOHOL,
+			TypeMapper.getDatatypeURI(Integer.class), 1, 1));
 	oci.addDatatypeProperty(HealthProfile.PROP_S_TOBACCO).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			HealthProfile.PROP_S_TOBACCO, TypeMapper
-				.getDatatypeURI(Integer.class), 1, 1));
+			HealthProfile.PROP_S_TOBACCO,
+			TypeMapper.getDatatypeURI(Integer.class), 1, 1));
 	oci.addDatatypeProperty(HealthProfile.PROP_S_DRUGS).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			HealthProfile.PROP_S_DRUGS, TypeMapper
-				.getDatatypeURI(Integer.class), 1, 1));
+			HealthProfile.PROP_S_DRUGS,
+			TypeMapper.getDatatypeURI(Integer.class), 1, 1));
 	oci.addDatatypeProperty(HealthProfile.PROP_S_RECOMMENDED_WEIGHT)
 		.setFunctional();
 	oci.addRestriction(MergedRestriction
 
 	.getAllValuesRestrictionWithCardinality(
-		HealthProfile.PROP_S_RECOMMENDED_WEIGHT, TypeMapper
-			.getDatatypeURI(Integer.class), 1, 1));
+		HealthProfile.PROP_S_RECOMMENDED_WEIGHT,
+		TypeMapper.getDatatypeURI(Integer.class), 1, 1));
 	oci.addObjectProperty(HealthProfile.PROP_HAS_ILLNESS).setFunctional();
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
@@ -227,17 +225,16 @@ public final class HealthProfileOntology extends Ontology {
 	oci.addRestriction(MergedRestriction.getAllValuesRestriction(
 		HealthProfile.PROP_HAS_DISABILITY, AccessImpairment.MY_URI));
 	oci.addObjectProperty(HealthProfile.PROP_HAS_MEDICINE).setFunctional();
-	oci
-		.addRestriction(MergedRestriction
-			.getAllValuesRestrictionWithCardinality(
-				HealthProfile.PROP_HAS_MEDICINE,
-				Medicine.MY_URI, 1, 1));
-	
-	//Extend UserProfile
-	oci = extendExistingOntClassInfo(UserProfile.MY_URI);
-	oci.addObjectProperty(PROP_HEALTH_PROFILE).addSuperProperty(UserProfile.PROP_HAS_SUB_PROFILE);
 	oci.addRestriction(MergedRestriction
 		.getAllValuesRestrictionWithCardinality(
-			PROP_HEALTH_PROFILE, HealthProfile.MY_URI, 0, 1));
+			HealthProfile.PROP_HAS_MEDICINE, Medicine.MY_URI, 1, 1));
+
+	// Extend UserProfile
+	oci = extendExistingOntClassInfo(AssistedPersonProfile.MY_URI);
+	oci.addObjectProperty(PROP_HEALTH_PROFILE).addSuperProperty(
+		AssistedPersonProfile.PROP_HAS_SUB_PROFILE);
+	oci.addRestriction(MergedRestriction
+		.getAllValuesRestrictionWithCardinality(PROP_HEALTH_PROFILE,
+			HealthProfile.MY_URI, 0, 1));
     }
 }
