488d92480ff81d9f4b9026bb75554f2581c51a7e
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-03-08 18:20:11 +0000

    Automatic tag of ont.measurement version: 1.1.0

---
+++ measurement/src/main/java/org/universAAL/ontology/measurement/MultiDimensionMeasure.java
@@ -0,0 +1,55 @@
+/*	
+	Copyright 2008-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer Gesellschaft - Institut für Graphische Datenverarbeitung 
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ontology.measurement;
+
+import org.universAAL.middleware.owl.ManagedIndividual;
+
+/**
+ * See "Measurement.jpg" under src/main/resources.
+ * 
+ * @author mtazari - <a href="mailto:Saied.Tazari@igd.fraunhofer.de">Saied
+ *         Tazari</a>
+ * 
+ */
+public class MultiDimensionMeasure extends ManagedIndividual {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @seeorg.universAAL.middleware.context.owl.ManagedIndividual#
+     * getPropSerializationType(java.lang.String)
+     */
+    public int getPropSerializationType(String propURI) {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.universAAL.middleware.context.owl.ManagedIndividual#isWellFormed()
+     */
+    public boolean isWellFormed() {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+}
