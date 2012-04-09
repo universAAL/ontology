f6499bb275b1120044be54a1bd7f6514f2630905
Vadim Eisenberg <vadime@il.ibm.com>
2012-04-09 13:57:06 +0000

    create new trunk

---
+++ ontologies.core/ont.activityhub.core/src/main/java/org/universAAL/ontology/activityhub/FallSensor.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package org.universAAL.ontology.activityhub;
+
+import org.universAAL.ontology.ActivityHubOntology;
+
+/**
+ * Ontological representation of a fall sensor according to ISO 11073 - 
+ * Part 10471 (Indepentend living activity hub).
+ * Methods included in this class are the mandatory ones for
+ * representing an ontological concept in Java classes for uAAL. Usually it
+ * includes getters and setters for most of its properties.
+ * 
+ * Specific sensor events (from standard specification):
+ * - fall detected
+ * - no condition detected (optional)
+ * 
+ * TODO: Implement generic sensor properties flags for activity hub sensors
+ * 
+ * @author Thomas Fuxreiter
+ */
+public class FallSensor extends ActivityHubSensor {
+
+	public static final String MY_URI = ActivityHubOntology.NAMESPACE
+    + "FallSensor";
+
+	/**
+	 * 
+	 */
+	public FallSensor() {
+		//super();
+	}
+
+	/**
+	 * @param uri
+	 */
+	public FallSensor(String uri) {
+		super(uri);
+	}
+
+    /**
+     * From Resource class:
+     * The properties denote the RDF triples of this resource, realized as
+     * Hashtable. The RDF subject is this Resource itself, the key of the
+     * Hashtable is the RDF predicate and the value of the Hashtable is the RDF
+     * object, which can be a literal or another resource. See
+     * {@link #setProperty(String propURI, Object value)} for more information.
+     */	
+	public void setMeasuredValue(FallSensorEvent event) {
+		props.put(PROP_MEASURED_VALUE, event);
+	}
+	
+    public FallSensorEvent getMeasuredvalue() {
+    	return ((FallSensorEvent) props.get(PROP_MEASURED_VALUE));
+    }
+	
+	/** 
+	 * @see org.universAAL.ontology.phThing.Sensor#getClassURI()
+	 * @return Ontology namespace for this class
+	 */
+	public String getClassURI() {
+		return MY_URI;
+	}
+	
+	public int getPropSerializationType(String propURI) {
+		return PROP_MEASURED_VALUE.equals(propURI) ? PROP_SERIALIZATION_FULL : 
+			super.getPropSerializationType(propURI);	
+	}
+
+	public boolean isWellFormed() {
+		return true;
+	}
+}
---
+++ ontologies.core/ont.profile.userid.core/src/main/java/org/universaal/ontology/useridprofileontology/owl/UserIDProfile.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright 2011 Ericsson Nikola Tesla d.d.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universaal.ontology.useridprofileontology.owl;
+
+import org.universAAL.ontology.profile.SubProfile;
+
+/**
+ * @author eandgrg
+ * 
+ */
+public class UserIDProfile extends SubProfile {
+	public static final String MY_URI = UserIDProfileOntology.NAMESPACE
+			+ "UserIDProfile";
+	public static final String PROP_USERNAME = UserIDProfileOntology.NAMESPACE
+			+ "USERNAME";
+	public static final String PROP_PASSWORD = UserIDProfileOntology.NAMESPACE
+			+ "PASSWORD";
+
+	public UserIDProfile() {
+		super();
+	}
+
+	public UserIDProfile(String uri) {
+		super(uri);
+	}
+
+	public String getClassURI() {
+		return MY_URI;
+	}
+
+	public int getPropSerializationType(String arg0) {
+		// TODO Implement or if for Device subclasses: remove
+		return 0;
+	}
+
+	public boolean isWellFormed() {
+		return true && props.containsKey(PROP_USERNAME)
+				&& props.containsKey(PROP_PASSWORD);
+	}
+
+	public String getUSERNAME() {
+		return (String) props.get(PROP_USERNAME);
+	}
+
+	public void setUSERNAME(String newPropValue) {
+		if (newPropValue != null)
+			props.put(PROP_USERNAME, newPropValue);
+	}
+
+	public String getPASSWORD() {
+		return (String) props.get(PROP_PASSWORD);
+	}
+
+	public void setPASSWORD(String newPropValue) {
+		if (newPropValue != null)
+			props.put(PROP_PASSWORD, newPropValue);
+	}
+}
