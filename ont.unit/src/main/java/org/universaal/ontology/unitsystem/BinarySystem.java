2af06789f555933e2c5ee03bcd47d1845a3d768b
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-06-20 23:36:06 +0000

    merging to trunk

---
+++ ont.unit/src/main/java/org/universaal/ontology/unitsystem/BinarySystem.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright 2012 UPM - Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universaal.ontology.unitsystem;
+
+import org.universaal.ontology.UnitOntology;
+import org.universaal.ontology.owl.MeasurableDimension;
+import org.universaal.ontology.owl.Prefix;
+import org.universaal.ontology.owl.Unit;
+import org.universaal.ontology.owl.UnitSystem;
+
+public class BinarySystem extends UnitSystem {
+  
+	public static final String MY_URI = UnitOntology.NAMESPACE
+    + "BinarySystem";
+  
+  public static UnitSystem IND_BS;
+  public static Unit IND_UNIT_BS_BIT;
+  public static Unit IND_UNIT_BS_BYTE;
+  public static Prefix IND_PREFIX_BS_KIBI;
+  public static Prefix IND_PREFIX_BS_MEBI;
+  public static Prefix IND_PREFIX_BS_GIBI;
+  public static Prefix IND_PREFIX_BS_TEBI;
+  public static Prefix IND_PREFIX_BS_PEBI;
+  public static Prefix IND_PREFIX_BS_EXBI;
+  public static Prefix IND_PREFIX_BS_ZEBI;
+  public static Prefix IND_PREFIX_BS_YOBI;
+  
+
+  {
+	  IND_BS = new BinarySystem("binarySytem");
+	  
+	  IND_UNIT_BS_BIT = new Unit("bit", "Bit", "b", MeasurableDimension.ComputerStorage, IND_BS);
+	  IND_UNIT_BS_BYTE = new Unit("byte", "Byte", "B", MeasurableDimension.ComputerStorage, IND_BS);
+	  
+	  addUnits(IND_UNIT_BS_BIT);
+	  addUnits(IND_UNIT_BS_BYTE);
+	  
+	  IND_PREFIX_BS_KIBI = newBinaryPrefix("Kibi", "ki", 10);
+	  IND_PREFIX_BS_MEBI = newBinaryPrefix("Mebi", "Mi", 20);
+	  IND_PREFIX_BS_GIBI = newBinaryPrefix("Gibi", "Gi", 30);
+	  IND_PREFIX_BS_TEBI = newBinaryPrefix("Tebi", "Ti", 40);
+	  IND_PREFIX_BS_PEBI = newBinaryPrefix("Pebi", "Pi", 50);
+	  IND_PREFIX_BS_EXBI = newBinaryPrefix("Exbi", "Ei", 60);
+	  IND_PREFIX_BS_ZEBI = newBinaryPrefix("Zebi", "Zi", 70);
+	  IND_PREFIX_BS_YOBI = newBinaryPrefix("Yobi", "Yi", 80);
+  }
+  
+  public BinarySystem () {
+    super();
+  }
+
+  public BinarySystem (String uri) {
+    super(uri,"Binary System");
+  }
+
+  public String getClassURI() {
+    return MY_URI;
+  }
+  public int getPropSerializationType(String arg0) {
+	
+	return PROP_SERIALIZATION_FULL;
+  }
+
+  public boolean isWellFormed() {
+	return true ;
+  }
+ 
+  private Prefix newBinaryPrefix(String name, String symb, int power) {
+	  Prefix p = new Prefix(name.toLowerCase(), name, symb, 2, power);
+		addPrefixes(p);
+		return p;
+  }
+}
---
+++ ont.unit/src/main/java/org/universaal/ontology/unitsystem/Util.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright 2012 UPM - Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universaal.ontology.unitsystem;
+
+import org.universaal.ontology.UnitOntology;
+import org.universaal.ontology.owl.DividedUnit;
+import org.universaal.ontology.owl.MeasurableDimension;
+import org.universaal.ontology.owl.MultipliedUnit;
+import org.universaal.ontology.owl.Unit;
+
+public class Util {
+  
+	public static final String MY_URI = UnitOntology.NAMESPACE
+    + "UtilUnits";
+  
+  public static Unit IND_UNIT_UNITY;
+  public static Unit IND_UNIT_HERTZ;
+  public static Unit IND_UNIT_VOLT;
+  
+
+  {
+	  
+	  IND_UNIT_UNITY = new Unit("unity", "Unity", "1", MeasurableDimension.Adiemnsional);
+	  IND_UNIT_HERTZ = new DividedUnit("hertz", IND_UNIT_UNITY, InternationalSystem.IND_UNIT_SI_SECOND);
+	  IND_UNIT_VOLT = new DividedUnit("volt",
+			  new MultipliedUnit("kg*m2", 
+					  InternationalSystem.IND_UNIT_SI_GRAM, InternationalSystem.IND_PREFIX_SI_KILO,
+					  new MultipliedUnit("m2", InternationalSystem.IND_UNIT_SI_METER, InternationalSystem.IND_UNIT_SI_METER))
+	  , new MultipliedUnit("A*S3", InternationalSystem.IND_UNIT_SI_AMPERE, 
+			  new MultipliedUnit("S3", InternationalSystem.IND_UNIT_SI_SECOND, 
+					  new MultipliedUnit("s2", InternationalSystem.IND_UNIT_SI_SECOND, InternationalSystem.IND_UNIT_SI_SECOND))));
+	  IND_UNIT_VOLT.setProperty(Unit.PROP_SYMBOL, "V");
+  }
+}
