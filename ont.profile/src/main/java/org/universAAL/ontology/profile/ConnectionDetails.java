99e9764381591e47f84e86463897368577b2754e
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-07-14 12:14:09 +0000

    Automatic tag of ont.profile version: 1.2.0

---
+++ ont.profile/src/main/java/org/universAAL/ontology/profile/ConnectionDetails.java
@@ -0,0 +1,36 @@
+package org.universAAL.ontology.profile;
+
+import org.universAAL.middleware.owl.ManagedIndividual;
+
+public class ConnectionDetails extends ManagedIndividual {
+
+    public static final String MY_URI = ProfileOntology.NAMESPACE
+	    + "ConnectionDetails";
+
+    public static final String PROP_HOST = ProfileOntology.NAMESPACE
+	    + "hasHost";
+    public static final String PROP_PORT = ProfileOntology.NAMESPACE
+	    + "hasPort";
+    public static final String PROP_URL = ProfileOntology.NAMESPACE + "hasURL";
+
+    protected ConnectionDetails() {
+	super();
+    }
+
+    public ConnectionDetails(String uri) {
+	super(uri);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+
+    public int getPropSerializationType(String propURI) {
+	return PROP_SERIALIZATION_FULL;
+    }
+
+}
---
+++ ont.profile/src/main/java/org/universAAL/ontology/profile/OntologyEntry.java
@@ -0,0 +1,36 @@
+package org.universAAL.ontology.profile;
+
+import org.universAAL.middleware.owl.ManagedIndividual;
+
+public class OntologyEntry extends ManagedIndividual {
+
+    public static final String MY_URI = ProfileOntology.NAMESPACE
+	    + "OntologyEntry";
+    public static final String PROP_NAMESPACE = ProfileOntology.NAMESPACE
+	    + "hasNamespace";
+    public static final String PROP_ONTOLOGY_URI = ProfileOntology.NAMESPACE
+	    + "hasOntologyURI";
+    public static final String PROP_VERSION = ProfileOntology.NAMESPACE
+	    + "hasOntVersion";
+
+    protected OntologyEntry() {
+	super();
+    }
+
+    public OntologyEntry(String uri) {
+	super(uri);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+
+    public int getPropSerializationType(String propURI) {
+	return PROP_SERIALIZATION_FULL;
+    }
+
+}
