f55476c071f8844fe354c92abe86a7d86b6c0ee6
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-07-14 12:16:17 +0000

    Automatic tag of ont.che version: 1.2.0

---
+++ ont.che/src/main/java/org/universAAL/ontology/ContextHistoryActivator.java
@@ -0,0 +1,19 @@
+package org.universAAL.ontology;
+
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.uAALModuleActivator;
+import org.universAAL.middleware.owl.OntologyManagement;
+import org.universAAL.ontology.che.ContextHistoryOntology;
+
+public class ContextHistoryActivator implements uAALModuleActivator {
+    private ContextHistoryOntology ont=new ContextHistoryOntology();
+
+    public void start(ModuleContext bcontext) throws Exception {
+	OntologyManagement.getInstance().register(ont);
+    }
+
+    public void stop(ModuleContext arg0) throws Exception {
+	OntologyManagement.getInstance().unregister(ont);
+    }
+
+}
