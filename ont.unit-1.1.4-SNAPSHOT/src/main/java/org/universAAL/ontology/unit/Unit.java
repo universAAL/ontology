1475493817bb2934c25087a610a9a070fa22f265
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-07-14 11:48:22 +0000

    Automatic tag of ont.unit version: 1.1.4-SNAPSHOT

---
+++ ont.unit-1.1.4-SNAPSHOT/src/main/java/org/universAAL/ontology/unit/Unit.java
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ * Copyright 2012 UPM - Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology.unit;
+
+import org.universAAL.middleware.owl.ManagedIndividual;
+
+public class Unit extends ManagedIndividual {
+  public static final String MY_URI = UnitOntology.NAMESPACE
+    + "Unit";
+  public static final String PROP_UNIT_SYSTEM = UnitOntology.NAMESPACE
+    + "unitSystem";
+  public static final String PROP_NAME = UnitOntology.NAMESPACE
+    + "name";
+  public static final String PROP_SYMBOL = UnitOntology.NAMESPACE
+    + "symbol";
+  public static final String PROP_DIMENSION = UnitOntology.NAMESPACE
+    + "dimension";
+
+
+  public Unit () {
+    super();
+  }
+  
+  public Unit (String uri) {
+    super(uri);
+  }
+  
+  public Unit(String uri, String name, String symbol, MeasurableDimension dimension){
+	  super(uri);
+	  setName(name);
+	  setSymbol(symbol);
+	  setDimension(dimension);
+  }
+
+  public Unit(String uri, String name, String symbol, MeasurableDimension dimension, UnitSystem us){
+	  super(uri);
+	  setName(name);
+	  setSymbol(symbol);
+	  setDimension(dimension);
+	  setUnitSustem(us);
+  }
+  
+  public String getClassURI() {
+    return MY_URI;
+  }
+  public int getPropSerializationType(String arg0) {
+	
+	return PROP_SERIALIZATION_FULL;
+  }
+
+  public boolean isWellFormed() {
+	return true 
+      && hasProperty(PROP_NAME)
+      && hasProperty(PROP_SYMBOL)
+      && hasProperty(PROP_DIMENSION);
+  }
+
+  public String getName() {
+    return (String)getProperty(PROP_NAME);
+  }		
+
+  private void setName(String newPropValue) {
+    if (newPropValue != null)
+      setProperty(PROP_NAME, newPropValue);
+  }		
+
+  public String getSymbol() {
+    return (String)getProperty(PROP_SYMBOL);
+  }		
+
+  private void setSymbol(String newPropValue) {
+    if (newPropValue != null)
+      setProperty(PROP_SYMBOL, newPropValue);
+  }		
+
+  public MeasurableDimension getDimension() {
+    return (MeasurableDimension)getProperty(PROP_DIMENSION);
+  }		
+
+  private void setDimension(MeasurableDimension newPropValue) {
+    if (newPropValue != null)
+      setProperty(PROP_DIMENSION, newPropValue);
+  }
+  
+  public UnitSystem getUnitSystem(){
+	  return (UnitSystem)getProperty(PROP_UNIT_SYSTEM);
+  }
+  
+  private void setUnitSustem(UnitSystem newPropValue){
+	  if (newPropValue != null)
+	      setProperty(PROP_DIMENSION, newPropValue);
+  }
+}
