74f396231bb8b0420a2eeda76e26aa36722d0346
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-07-14 12:14:56 +0000

    Automatic tag of ont.unit version: 1.2.0

---
+++ ont.unit/src/main/java/org/universAAL/ontology/UnitFactory.java
@@ -0,0 +1,30 @@
+package org.universAAL.ontology;
+
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.rdf.impl.ResourceFactoryImpl;
+import org.universAAL.ontology.unit.DividedUnit;
+import org.universAAL.ontology.unit.MultipliedUnit;
+import org.universAAL.ontology.unit.Prefix;
+import org.universAAL.ontology.unit.Unit;
+import org.universAAL.ontology.unit.UnitSystem;
+
+public class UnitFactory extends ResourceFactoryImpl {
+
+    public Resource createInstance(String classURI, String instanceURI,
+	    int factoryIndex) {
+	switch (factoryIndex) {
+	case 1:
+	    return new Unit(instanceURI);
+	case 2:
+	    return new UnitSystem(instanceURI);
+	case 3:
+	    return new Prefix(instanceURI);
+	case 4:
+	    return new MultipliedUnit(instanceURI);
+	case 5:
+	    return new DividedUnit(instanceURI);
+	}
+	return null;
+    }
+
+}
