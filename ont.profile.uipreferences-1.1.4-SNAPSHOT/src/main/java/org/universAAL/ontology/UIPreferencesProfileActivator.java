cad5a726c6b5aeed37f1a46a679ce147fe080f4e
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-07-14 11:48:55 +0000

    Automatic tag of ont.profile.uipreferences version: 1.1.4-SNAPSHOT

---
+++ ont.profile.uipreferences-1.1.4-SNAPSHOT/src/main/java/org/universAAL/ontology/UIPreferencesProfileActivator.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright 2011 Ericsson Nikola Tesla d.d.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology;
+
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.uAALModuleActivator;
+import org.universAAL.middleware.owl.OntologyManagement;
+import org.universAAL.ontology.profile.uipreferences.UIPreferencesProfileOntology;
+
+/**
+ * @author eandgrg
+ *
+ */
+public class UIPreferencesProfileActivator implements uAALModuleActivator {
+
+  UIPreferencesProfileOntology ontology = new UIPreferencesProfileOntology();
+
+  /* (non-Javadoc)
+ * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+ */
+public void start(ModuleContext context) throws Exception {
+    OntologyManagement.getInstance().register(ontology);
+  }
+
+  /* (non-Javadoc)
+ * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+ */
+public void stop(ModuleContext arg0) throws Exception {
+    OntologyManagement.getInstance().unregister(ontology);
+  }
+}	
