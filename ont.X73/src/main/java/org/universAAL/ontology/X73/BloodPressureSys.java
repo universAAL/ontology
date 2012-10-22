7d4d10f09a88df3f6cfe4a91e00588a43a3231af
DI(FH) Thomas Fuxreiter <thomas.fuxreiter@ait.ac.at>
2012-10-22 11:16:45 +0000

    Added new Ontology for ISO 11073 devices WeighingScale and BloodPressure; created with Ontology Creator (Eclipse Plugin) from WP3.

---
+++ ont.X73/src/main/java/org/universAAL/ontology/X73/BloodPressureSys.java
@@ -0,0 +1,34 @@
+package org.universAAL.ontology.X73;
+
+import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.universAAL.middleware.owl.ManagedIndividual;
+
+
+public class BloodPressureSys extends MDSAttribute {
+  public static final String MY_URI = X73Ontology.NAMESPACE
+    + "BloodPressureSys";
+
+
+  public BloodPressureSys () {
+    super();
+  }
+  
+  public BloodPressureSys (String uri) {
+    super(uri);
+  }
+
+  public String getClassURI() {
+    return MY_URI;
+  }
+
+  public int getPropSerializationType(String propURI) {
+	return super.getPropSerializationType(propURI);
+  }
+
+  public boolean isWellFormed() {
+	return true ;
+  }
+}
