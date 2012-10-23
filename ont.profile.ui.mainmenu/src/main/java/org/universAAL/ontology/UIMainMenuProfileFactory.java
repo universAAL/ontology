e7015b13d20d047ffc15d143101d730019da954f
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-10-23 23:39:03 +0000

    started with main menu profile

---
+++ ont.profile.ui.mainmenu/src/main/java/org/universAAL/ontology/UIMainMenuProfileFactory.java
@@ -0,0 +1,37 @@
+/*	
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institut für Graphische Datenverarbeitung
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ontology;
+
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.rdf.impl.ResourceFactoryImpl;
+import org.universAAL.ontology.profile.ui.mainmenu.UIMainMenuProfile;
+
+public class UIMainMenuProfileFactory extends ResourceFactoryImpl {
+
+    public Resource createInstance(String classURI, String instanceURI,
+	    int factoryIndex) {
+
+	switch (factoryIndex) {
+	case 0:
+	    return new UIMainMenuProfile(instanceURI);
+	}
+	return null;
+    }
+}
---
+++ ont.profile.ui.mainmenu/src/main/java/org/universAAL/ontology/profile/ui/mainmenu/UIMainMenuProfile.java
@@ -0,0 +1,40 @@
+/*	
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institut für Graphische Datenverarbeitung
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ontology.profile.ui.mainmenu;
+
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.ontology.profile.SubProfile;
+
+public class UIMainMenuProfile extends SubProfile {
+    public static final String MY_URI = UIMainMenuProfileOntology.NAMESPACE
+	    + "UIMainMenuProfile";
+
+    public UIMainMenuProfile(String uri) {
+	super(uri);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public int getPropSerializationType(String arg0) {
+	return Resource.PROP_SERIALIZATION_FULL;
+    }
+}
