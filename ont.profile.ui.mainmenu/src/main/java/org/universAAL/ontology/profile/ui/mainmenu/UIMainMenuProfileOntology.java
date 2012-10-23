c02151d123a44034394a2fe34cedd14f2d9a0e0d
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-10-23 23:39:03 +0000

    started with main menu profile

---
+++ ont.profile.ui.mainmenu/src/main/java/org/universAAL/ontology/profile/ui/mainmenu/UIMainMenuProfileOntology.java
@@ -0,0 +1,67 @@
+/*	
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institut für Graphische Datenverarbeitung
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ontology.profile.ui.mainmenu;
+
+import org.universAAL.middleware.owl.DataRepOntology;
+import org.universAAL.middleware.owl.MergedRestriction;
+import org.universAAL.middleware.owl.OntClassInfoSetup;
+import org.universAAL.middleware.owl.Ontology;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.ontology.UIMainMenuProfileFactory;
+import org.universAAL.ontology.profile.Profile;
+import org.universAAL.ontology.profile.SubProfile;
+import org.universAAL.ontology.profile.UserProfile;
+
+public final class UIMainMenuProfileOntology extends Ontology {
+
+    private static UIMainMenuProfileFactory factory = new UIMainMenuProfileFactory();
+    public static final String NAMESPACE = "http://ontology.universaal.org/UIMainMenuProfile.owl#";
+
+    public static final String PROP_UI_MAINMENU_PROFILE = Profile.PROP_HAS_SUB_PROFILE
+	    + "hasUIMainMenu";
+
+    public UIMainMenuProfileOntology() {
+	super(NAMESPACE);
+    }
+
+    public void create() {
+	Resource r = getInfo();
+	r
+		.setResourceComment("A profile describing the main menu shown by the user interaction framework.");
+	r.setResourceLabel("UI Main Menu Profile");
+	addImport(DataRepOntology.NAMESPACE);
+
+	// ******* Declaration of regular classes of the ontology ******* //
+	OntClassInfoSetup oci = createNewOntClassInfo(UIMainMenuProfile.MY_URI,
+		factory, 0);
+	oci.setResourceComment("");
+	oci.setResourceLabel("Main Menu");
+	oci.addSuperClass(SubProfile.MY_URI);
+
+	// Extend UserProfile
+	oci = extendExistingOntClassInfo(UserProfile.MY_URI);
+	oci.addObjectProperty(PROP_UI_MAINMENU_PROFILE).addSuperProperty(
+		Profile.PROP_HAS_SUB_PROFILE);
+	oci.addRestriction(MergedRestriction
+		.getAllValuesRestrictionWithCardinality(
+			PROP_UI_MAINMENU_PROFILE, UIMainMenuProfile.MY_URI, 0,
+			1));
+    }
+}
