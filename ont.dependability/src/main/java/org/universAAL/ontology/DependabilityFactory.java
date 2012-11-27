bce53148d7cf1aeff17673e9135b553a38f85a38
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-11-27 00:09:44 +0000

    removed unused osgi imports

--- ont.dependability/src/main/java/org/universAAL/ontology/DependabilityFactory.java
+++ ont.dependability/src/main/java/org/universAAL/ontology/DependabilityFactory.java
@@ -1,8 +1,6 @@
 
 package org.universAAL.ontology;
 
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
 import org.universAAL.middleware.rdf.Resource;
 import org.universAAL.middleware.rdf.impl.ResourceFactoryImpl;
 
