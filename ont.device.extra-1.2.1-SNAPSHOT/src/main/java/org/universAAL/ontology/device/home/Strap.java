2ded7ade0eca015928677ef50d5b71f82710403a
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-11-28 12:44:40 +0000

    Automatic tag of ont.device.extra version: 1.2.1-SNAPSHOT

---
+++ ont.device.extra-1.2.1-SNAPSHOT/src/main/java/org/universAAL/ontology/device/home/Strap.java
@@ -0,0 +1,60 @@
+/*
+	Copyright 2008-2014 ITACA-TSB, http://www.tsb.upv.es
+	Instituto Tecnologico de Aplicaciones de Comunicacion 
+	Avanzadas - Grupo Tecnologias para la Salud y el 
+	Bienestar (TSB)
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ontology.device.home;
+
+import org.universAAL.ontology.phThing.Device;
+
+/**
+ * Ontological representation of any device that can be attached to a person by
+ * a strap. Methods included in this class are the mandatory ones for
+ * representing an ontological concept in Java classes for uAAL. Currently this
+ * concept is just a placehodler and does not contain getters nor setters of its
+ * own, as it has no specific properties.
+ * 
+ * @author <a href="mailto:alfiva@itaca.upv.es">Alvaro Fides Valero</a>
+ * 
+ */
+public class Strap extends Device {
+
+    /** Class URI */
+    public static final String MY_URI = Device.uAAL_DEVICE_NAMESPACE + "Strap";
+
+    public Strap() {
+    }
+
+    public Strap(String uri) {
+	super(uri);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+
+    public int getPropSerializationType(String propURI) {
+	return PROP_SERIALIZATION_FULL;
+    }
+
+}
