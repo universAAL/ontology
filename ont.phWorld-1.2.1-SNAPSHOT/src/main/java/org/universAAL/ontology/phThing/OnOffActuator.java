c34be8c7b5090f031cdd0a2994d92a5d1cb4022f
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-11-28 12:44:30 +0000

    Automatic tag of ont.phWorld version: 1.2.1-SNAPSHOT

---
+++ ont.phWorld-1.2.1-SNAPSHOT/src/main/java/org/universAAL/ontology/phThing/OnOffActuator.java
@@ -0,0 +1,68 @@
+/*
+	Copyright 2008-2014 ITACA-TSB, http://www.tsb.upv.es
+	Instituto Tecnologico de Aplicaciones de Comunicacion 
+	Avanzadas - Grupo Tecnologias para la Salud y el 
+	Bienestar (TSB)
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ontology.phThing;
+
+/**
+ * Ontological representation of a binary actuator that can be turned on or off.
+ * Methods included in this class are the mandatory ones for representing an
+ * ontological concept in Java classes for uAAL. Usually it includes getters and
+ * setters for most of its properties.
+ * 
+ * @author <a href="mailto:alfiva@itaca.upv.es">Alvaro Fides Valero</a>
+ * 
+ */
+public class OnOffActuator extends Actuator {
+    public static final String MY_URI;
+    public static final String PROP_STATUS;
+
+    static {
+	MY_URI = Device.uAAL_DEVICE_NAMESPACE + "OnOffActuator";
+	PROP_STATUS = Device.uAAL_DEVICE_NAMESPACE + "Status";
+    }
+
+    public OnOffActuator() {
+    }
+
+    public OnOffActuator(String uri) {
+	super(uri);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public int getPropSerializationType(String propURI) {
+	return PROP_SERIALIZATION_FULL;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+
+    public boolean getStatus() {
+	return ((Boolean) props.get(PROP_STATUS)).booleanValue();
+    }
+
+    public void setStatus(boolean value) {
+	props.put(PROP_STATUS, Boolean.valueOf(value));
+    }
+}
