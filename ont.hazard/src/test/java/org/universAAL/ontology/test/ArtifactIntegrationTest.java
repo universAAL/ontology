a8319d2f8ac5659d7e4dbd49c9ae7557c0a24c35
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-11-28 13:05:14 +0000

    Automatic tag of ont.hazard version: 1.3.0

---
+++ ont.hazard/src/test/java/org/universAAL/ontology/test/ArtifactIntegrationTest.java
@@ -0,0 +1,18 @@
+package org.universAAL.ontology.test;
+
+import org.universAAL.itests.IntegrationTest;
+
+/**
+ * Here developer's of this artifact should code their integration tests.
+ * 
+ * @author rotgier
+ * 
+ */
+public class ArtifactIntegrationTest extends IntegrationTest {
+
+
+    public void testComposite() {
+	logAllBundles();
+    }
+
+}
