780e70400c2e6801e579b286a5b38b1d7acae2f1
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-11-28 13:06:41 +0000

    Automatic tag of ont.dependability version: 1.3.0

---
+++ ont.dependability/src/main/java/org/universAAL/ontology/dependability/IndirectFault.java
@@ -0,0 +1,34 @@
+package org.universAAL.ontology.dependability;
+
+import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.universAAL.middleware.owl.ManagedIndividual;
+
+
+public class IndirectFault extends Fault {
+  public static final String MY_URI = DependabilityOntology.NAMESPACE
+    + "IndirectFault";
+
+
+  public IndirectFault () {
+    super();
+  }
+  
+  public IndirectFault (String uri) {
+    super(uri);
+  }
+
+  public String getClassURI() {
+    return MY_URI;
+  }
+  public int getPropSerializationType(String arg0) {
+	// TODO Implement or if for Device subclasses: remove 
+	return 0;
+  }
+
+  public boolean isWellFormed() {
+	return true ;
+  }
+}
