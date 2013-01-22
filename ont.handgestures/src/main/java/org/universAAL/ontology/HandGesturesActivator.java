b24c04ac93d0f748e50e2e386802acb3c4078db6
mr Andrej Grguric <andrej.grguric@ericsson.com>
2013-01-22 08:55:42 +0000

    ont handgestures, v1.3.1-S upload

---
+++ ont.handgestures/src/main/java/org/universAAL/ontology/HandGesturesActivator.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright 2012 Ericsson Nikola Tesla d.d.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology;
+
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.ModuleActivator;
+import org.universAAL.middleware.owl.OntologyManagement;
+import org.universAAL.ontology.handgestures.HandgesturesOntology;
+
+
+/**
+ * 
+ * @author eandgrg
+ * 
+ */
+
+public class HandGesturesActivator implements ModuleActivator {
+
+	HandgesturesOntology _handgesturesontology = new HandgesturesOntology();
+
+	public void start(ModuleContext context) throws Exception {
+		OntologyManagement.getInstance().register(_handgesturesontology);
+
+	}
+
+	public void stop(ModuleContext arg0) throws Exception {
+
+		OntologyManagement.getInstance().unregister(_handgesturesontology);
+
+	}
+}
