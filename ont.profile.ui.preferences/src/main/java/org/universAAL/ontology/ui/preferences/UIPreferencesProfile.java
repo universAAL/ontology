85449028fd5a51e25c782db6c37f716efd02307a
mr Andrej Grguric <andrej.grguric@ericsson.com>
2013-02-25 10:53:27 +0000

    orphan property deleted (probably stayed hidden in ont modeling tool before automatic generation of ont)

--- ont.profile.ui.preferences/src/main/java/org/universAAL/ontology/ui/preferences/UIPreferencesProfile.java
+++ ont.profile.ui.preferences/src/main/java/org/universAAL/ontology/ui/preferences/UIPreferencesProfile.java
@@ -17,112 +17,100 @@ package org.universAAL.ontology.ui.preferences;
 import org.universAAL.middleware.owl.ManagedIndividual;
 import org.universAAL.middleware.rdf.Resource;
 
-
 public class UIPreferencesProfile extends ManagedIndividual {
-  public static final String MY_URI = UIPreferencesProfileOntology.NAMESPACE
-    + "UIPreferencesProfile";
-  public static final String PROP_INTERACTION_PREFERENCES = UIPreferencesProfileOntology.NAMESPACE
-    + "interactionPreferences";
-  public static final String PROP_VISUAL_PREFERENCES = UIPreferencesProfileOntology.NAMESPACE
-    + "visualPreferences";
-  public static final String PROP_SYSTEM_MENU_PREFERENCES = UIPreferencesProfileOntology.NAMESPACE
-    + "systemMenuPreferences";
-  public static final String PROP_ALERT_PREFERENCES = UIPreferencesProfileOntology.NAMESPACE
-    + "alertPreferences";
-  public static final String PROP_ADAPTATION_PREFERENCES = UIPreferencesProfileOntology.NAMESPACE
-    + "adaptationPreferences";
-  public static final String PROP_ACCESS_MODE = UIPreferencesProfileOntology.NAMESPACE
-    + "accessMode";
-  public static final String PROP_AUDIO_PREFERENCES = UIPreferencesProfileOntology.NAMESPACE
-    + "audioPreferences";
+    public static final String MY_URI = UIPreferencesProfileOntology.NAMESPACE
+	    + "UIPreferencesProfile";
+    public static final String PROP_INTERACTION_PREFERENCES = UIPreferencesProfileOntology.NAMESPACE
+	    + "interactionPreferences";
+    public static final String PROP_VISUAL_PREFERENCES = UIPreferencesProfileOntology.NAMESPACE
+	    + "visualPreferences";
+    public static final String PROP_SYSTEM_MENU_PREFERENCES = UIPreferencesProfileOntology.NAMESPACE
+	    + "systemMenuPreferences";
+    public static final String PROP_ALERT_PREFERENCES = UIPreferencesProfileOntology.NAMESPACE
+	    + "alertPreferences";
+    public static final String PROP_ACCESS_MODE = UIPreferencesProfileOntology.NAMESPACE
+	    + "accessMode";
+    public static final String PROP_AUDIO_PREFERENCES = UIPreferencesProfileOntology.NAMESPACE
+	    + "audioPreferences";
 
+    public UIPreferencesProfile() {
+	super();
+    }
 
-  public UIPreferencesProfile () {
-    super();
-  }
-  
-  public UIPreferencesProfile (String uri) {
-    super(uri);
-  }
+    public UIPreferencesProfile(String uri) {
+	super(uri);
+    }
 
-  public String getClassURI() {
-    return MY_URI;
-  }
-  public int getPropSerializationType(String arg0) {
-      return Resource.PROP_SERIALIZATION_FULL;
-  }
+    public String getClassURI() {
+	return MY_URI;
+    }
 
-  public boolean isWellFormed() {
-	return true 
-      && hasProperty(PROP_INTERACTION_PREFERENCES)
-      && hasProperty(PROP_VISUAL_PREFERENCES)
-      && hasProperty(PROP_SYSTEM_MENU_PREFERENCES)
-      && hasProperty(PROP_ALERT_PREFERENCES)
-      && hasProperty(PROP_ADAPTATION_PREFERENCES)
-      && hasProperty(PROP_ACCESS_MODE)
-      && hasProperty(PROP_AUDIO_PREFERENCES);
-  }
+    public int getPropSerializationType(String arg0) {
+	return Resource.PROP_SERIALIZATION_FULL;
+    }
 
-  public AlertPreferences getAlertPreferences() {
-    return (AlertPreferences)getProperty(PROP_ALERT_PREFERENCES);
-  }		
+    public boolean isWellFormed() {
+	return true && hasProperty(PROP_INTERACTION_PREFERENCES)
+		&& hasProperty(PROP_VISUAL_PREFERENCES)
+		&& hasProperty(PROP_SYSTEM_MENU_PREFERENCES)
+		&& hasProperty(PROP_ALERT_PREFERENCES)
+		&& hasProperty(PROP_ACCESS_MODE)
+		&& hasProperty(PROP_AUDIO_PREFERENCES);
+    }
 
-  public void setAlertPreferences(AlertPreferences newPropValue) {
-    if (newPropValue != null)
-      changeProperty(PROP_ALERT_PREFERENCES, newPropValue);
-  }		
+    public AlertPreferences getAlertPreferences() {
+	return (AlertPreferences) getProperty(PROP_ALERT_PREFERENCES);
+    }
 
-  public VisualPreferences getVisualPreferences() {
-    return (VisualPreferences)getProperty(PROP_VISUAL_PREFERENCES);
-  }		
+    public void setAlertPreferences(AlertPreferences newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_ALERT_PREFERENCES, newPropValue);
+    }
 
-  public void setVisualPreferences(VisualPreferences newPropValue) {
-    if (newPropValue != null)
-      changeProperty(PROP_VISUAL_PREFERENCES, newPropValue);
-  }		
+    public VisualPreferences getVisualPreferences() {
+	return (VisualPreferences) getProperty(PROP_VISUAL_PREFERENCES);
+    }
 
-  public AuditoryPreferences getAudioPreferences() {
-    return (AuditoryPreferences)getProperty(PROP_AUDIO_PREFERENCES);
-  }		
+    public void setVisualPreferences(VisualPreferences newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_VISUAL_PREFERENCES, newPropValue);
+    }
 
-  public void setAudioPreferences(AuditoryPreferences newPropValue) {
-    if (newPropValue != null)
-      changeProperty(PROP_AUDIO_PREFERENCES, newPropValue);
-  }		
+    public AuditoryPreferences getAudioPreferences() {
+	return (AuditoryPreferences) getProperty(PROP_AUDIO_PREFERENCES);
+    }
 
-  public GeneralInteractionPreferences getInteractionPreferences() {
-    return (GeneralInteractionPreferences)getProperty(PROP_INTERACTION_PREFERENCES);
-  }		
+    public void setAudioPreferences(AuditoryPreferences newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_AUDIO_PREFERENCES, newPropValue);
+    }
 
-  public void setInteractionPreferences(GeneralInteractionPreferences newPropValue) {
-    if (newPropValue != null)
-      changeProperty(PROP_INTERACTION_PREFERENCES, newPropValue);
-  }		
+    public GeneralInteractionPreferences getInteractionPreferences() {
+	return (GeneralInteractionPreferences) getProperty(PROP_INTERACTION_PREFERENCES);
+    }
 
-  public AccessMode getAccessMode() {
-    return (AccessMode)getProperty(PROP_ACCESS_MODE);
-  }		
+    public void setInteractionPreferences(
+	    GeneralInteractionPreferences newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_INTERACTION_PREFERENCES, newPropValue);
+    }
 
-  public void setAccessMode(AccessMode newPropValue) {
-    if (newPropValue != null)
-      changeProperty(PROP_ACCESS_MODE, newPropValue);
-  }		
+    public AccessMode getAccessMode() {
+	return (AccessMode) getProperty(PROP_ACCESS_MODE);
+    }
 
-  public SystemMenuPreferences getSystemMenuPreferences() {
-    return (SystemMenuPreferences)getProperty(PROP_SYSTEM_MENU_PREFERENCES);
-  }		
+    public void setAccessMode(AccessMode newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_ACCESS_MODE, newPropValue);
+    }
 
-  public void setSystemMenuPreferences(SystemMenuPreferences newPropValue) {
-    if (newPropValue != null)
-      changeProperty(PROP_SYSTEM_MENU_PREFERENCES, newPropValue);
-  }		
+    public SystemMenuPreferences getSystemMenuPreferences() {
+	return (SystemMenuPreferences) getProperty(PROP_SYSTEM_MENU_PREFERENCES);
+    }
 
-  public String getAdaptationPreferences() {
-    return (String)getProperty(PROP_ADAPTATION_PREFERENCES);
-  }		
+    public void setSystemMenuPreferences(SystemMenuPreferences newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_SYSTEM_MENU_PREFERENCES, newPropValue);
+    }
 
-  public void setAdaptationPreferences(String newPropValue) {
-    if (newPropValue != null)
-      changeProperty(PROP_ADAPTATION_PREFERENCES, newPropValue);
-  }		
 }
