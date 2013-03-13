e049523ac8f011d3c8a242700e23f6aa4f623206
Alvaro Fides <alfiva@itaca.upv.es>
2013-03-13 10:23:57 +0000

    Fixed inheritance of Light concepts

--- ont.device/src/main/java/org/universAAL/ontology/device/LightActuator.java
+++ ont.device/src/main/java/org/universAAL/ontology/device/LightActuator.java
@@ -21,7 +21,7 @@
  */
 package org.universAAL.ontology.device;
 
-public class LightActuator extends Sensor {
+public class LightActuator extends Actuator {
   public static final String MY_URI = DeviceOntology.NAMESPACE
     + "LightActuator";
   public static final String PROP_HAS_VALUE = DeviceOntology.NAMESPACE
