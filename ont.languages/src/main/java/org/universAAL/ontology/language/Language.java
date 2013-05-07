0b61db127ec5ce9fa1f1d3d9d8b171a04058cf72
mr Andrej Grguric <andrej.grguric@ericsson.com>
2013-05-07 13:12:53 +0000

    getPropSerializationType implemented (returns Full) for Language ont

--- ont.languages/src/main/java/org/universAAL/ontology/language/Language.java
+++ ont.languages/src/main/java/org/universAAL/ontology/language/Language.java
@@ -44,8 +44,7 @@ public class Language extends ManagedIndividual {
     }
 
     public int getPropSerializationType(String arg0) {
-	// TODO Implement or if for Device subclasses: remove
-	return 0;
+	return PROP_SERIALIZATION_FULL;
     }
 
     public boolean isWellFormed() {
