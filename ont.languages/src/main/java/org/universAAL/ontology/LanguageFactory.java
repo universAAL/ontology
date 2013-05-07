96f2392c93df79c44919ab31dbf2fba599eedf36
mr Andrej Grguric <andrej.grguric@ericsson.com>
2013-05-07 12:12:32 +0000

    licensing applied to new ont languages

--- ont.languages/src/main/java/org/universAAL/ontology/LanguageFactory.java
+++ ont.languages/src/main/java/org/universAAL/ontology/LanguageFactory.java
@@ -1,4 +1,18 @@
-
+/*******************************************************************************
+ * Copyright 2013 Universidad Politécnica de Madrid
+ * Copyright 2013 Ericsson Nikola Tesla d.d.
+ *
+ * Licensed under both Apache License, Version 2.0 and MIT License.
+ *
+ * See the NOTICE file distributed with this work for additional 
+ * information regarding copyright ownership
+ *	
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package org.universAAL.ontology;
 
 import java.io.BufferedReader;
@@ -10,50 +24,57 @@ import org.universAAL.middleware.rdf.Resource;
 import org.universAAL.middleware.rdf.impl.ResourceFactoryImpl;
 import org.universAAL.ontology.language.Language;
 
+/**
+ * @author amedrano
+ * @author eandgrg
+ * 
+ */
 public class LanguageFactory extends ResourceFactoryImpl {
 
-	public LanguageFactory(URL dataURL){
-		super();
-		tableURL = dataURL;
-	}
+    public LanguageFactory(URL dataURL) {
+	super();
+	tableURL = dataURL;
+    }
 
-	private URL tableURL;
-	
-	public Resource createInstance(String classURI, String instanceURI, int factoryIndex) {
-		Resource ret = null;
-		try {
-			BufferedReader br = new BufferedReader(
-					new InputStreamReader(tableURL.openStream(), Charset.forName("UTF-8")));
-			int line = 0;
-			String ll = "";
-			while (line != factoryIndex + 1
-					&& (ll = br.readLine()) != null){
-				line++;
-			}
-			if (ll != null) {
-				String[] props =  ll.split("\\|");
-				if (props.length >= 4) {
-					ret = new LanguageFactory.LanguageImpl(instanceURI, props[1], props[2], props[3]);					
-				}
-			}
-			br.close();
+    private URL tableURL;
+
+    public Resource createInstance(String classURI, String instanceURI,
+	    int factoryIndex) {
+	Resource ret = null;
+	try {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(
+		    tableURL.openStream(), Charset.forName("UTF-8")));
+	    int line = 0;
+	    String ll = "";
+	    while (line != factoryIndex + 1 && (ll = br.readLine()) != null) {
+		line++;
+	    }
+	    if (ll != null) {
+		String[] props = ll.split("\\|");
+		if (props.length >= 4) {
+		    ret = new LanguageFactory.LanguageImpl(instanceURI,
+			    props[1], props[2], props[3]);
 		}
-		catch (Exception e) {
-			// TODO: handle exception
-		}
+	    }
+	    br.close();
+	} catch (Exception e) {
+	    // TODO: handle exception
+	}
 	return ret;
-  }
-	/**
-	 * @author amedrano
-	 *
-	 */
-	public final class LanguageImpl extends Language {
+    }
 
-		public LanguageImpl(String uri, String name, String nativeName, String code){
-			super(uri);
-			setEnglishLabel(name);
-			setNativeLabel(nativeName);
-			setIso639code(code);
-		}
+    /**
+     * @author amedrano
+     * 
+     */
+    public final class LanguageImpl extends Language {
+
+	public LanguageImpl(String uri, String name, String nativeName,
+		String code) {
+	    super(uri);
+	    setEnglishLabel(name);
+	    setNativeLabel(nativeName);
+	    setIso639code(code);
 	}
+    }
 }
--- ont.languages/src/main/java/org/universAAL/ontology/language/Language.java
+++ ont.languages/src/main/java/org/universAAL/ontology/language/Language.java
@@ -1,66 +1,83 @@
+/*******************************************************************************
+ * Copyright 2013 Universidad Politécnica de Madrid
+ * Copyright 2013 Ericsson Nikola Tesla d.d.
+ *
+ * Licensed under both Apache License, Version 2.0 and MIT License.
+ *
+ * See the NOTICE file distributed with this work for additional 
+ * information regarding copyright ownership
+ *	
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package org.universAAL.ontology.language;
 
 import org.universAAL.middleware.owl.ManagedIndividual;
 
-
+/**
+ * @author amedrano
+ * @author eandgrg
+ * 
+ */
 public class Language extends ManagedIndividual {
-  public static final String MY_URI = LanguageOntology.NAMESPACE
-    + "Language";
-  public static final String PROP_ENGLISH_LABEL = LanguageOntology.NAMESPACE
-    + "englishLabel";
-  public static final String PROP_ISO639CODE = LanguageOntology.NAMESPACE
-    + "iso639code";
-  public static final String PROP_NATIVE_LABEL = LanguageOntology.NAMESPACE
-    + "nativeLabel";
+    public static final String MY_URI = LanguageOntology.NAMESPACE + "Language";
+    public static final String PROP_ENGLISH_LABEL = LanguageOntology.NAMESPACE
+	    + "englishLabel";
+    public static final String PROP_ISO639CODE = LanguageOntology.NAMESPACE
+	    + "iso639code";
+    public static final String PROP_NATIVE_LABEL = LanguageOntology.NAMESPACE
+	    + "nativeLabel";
 
+    public Language() {
+	super();
+    }
 
-  public Language () {
-    super();
-  }
-  
-  public Language (String uri) {
-    super(uri);
-  }
+    public Language(String uri) {
+	super(uri);
+    }
 
-  public String getClassURI() {
-    return MY_URI;
-  }
-  public int getPropSerializationType(String arg0) {
-	// TODO Implement or if for Device subclasses: remove 
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public int getPropSerializationType(String arg0) {
+	// TODO Implement or if for Device subclasses: remove
 	return 0;
-  }
+    }
 
-  public boolean isWellFormed() {
-	return true 
-      && hasProperty(PROP_ENGLISH_LABEL)
-      && hasProperty(PROP_ISO639CODE)
-      && hasProperty(PROP_NATIVE_LABEL);
-  }
+    public boolean isWellFormed() {
+	return true && hasProperty(PROP_ENGLISH_LABEL)
+		&& hasProperty(PROP_ISO639CODE)
+		&& hasProperty(PROP_NATIVE_LABEL);
+    }
 
-  public String getEnglishLabel() {
-    return (String)getProperty(PROP_ENGLISH_LABEL);
-  }		
+    public String getEnglishLabel() {
+	return (String) getProperty(PROP_ENGLISH_LABEL);
+    }
 
-  public void setEnglishLabel(String newPropValue) {
-    if (newPropValue != null)
-      changeProperty(PROP_ENGLISH_LABEL, newPropValue);
-  }		
+    public void setEnglishLabel(String newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_ENGLISH_LABEL, newPropValue);
+    }
 
-  public String getNativeLabel() {
-    return (String)getProperty(PROP_NATIVE_LABEL);
-  }		
+    public String getNativeLabel() {
+	return (String) getProperty(PROP_NATIVE_LABEL);
+    }
 
-  public void setNativeLabel(String newPropValue) {
-    if (newPropValue != null)
-      changeProperty(PROP_NATIVE_LABEL, newPropValue);
-  }		
+    public void setNativeLabel(String newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_NATIVE_LABEL, newPropValue);
+    }
 
-  public String getIso639code() {
-    return (String)getProperty(PROP_ISO639CODE);
-  }		
+    public String getIso639code() {
+	return (String) getProperty(PROP_ISO639CODE);
+    }
 
-  public void setIso639code(String newPropValue) {
-    if (newPropValue != null)
-      changeProperty(PROP_ISO639CODE, newPropValue);
-  }		
+    public void setIso639code(String newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_ISO639CODE, newPropValue);
+    }
 }
