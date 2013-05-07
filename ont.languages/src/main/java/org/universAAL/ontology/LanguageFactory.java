70137103c1b9213b97c27bcc63cc42f851e2972c
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-05-07 09:12:00 +0000

    dynamic Lanaguage creation (under testing).

--- ont.languages/src/main/java/org/universAAL/ontology/LanguageFactory.java
+++ ont.languages/src/main/java/org/universAAL/ontology/LanguageFactory.java
@@ -1,24 +1,43 @@
 
 package org.universAAL.ontology;
 
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
 import org.universAAL.middleware.rdf.Resource;
 import org.universAAL.middleware.rdf.impl.ResourceFactoryImpl;
-
-
-import org.universAAL.ontology.language.Language;
+import org.universAAL.ontology.language.LanguageOntology;
+import org.universAAL.ontology.language.impl.LanguageImpl;
 
 public class LanguageFactory extends ResourceFactoryImpl {
 
+	
 
-  public Resource createInstance(String classURI, String instanceURI, int factoryIndex) {
-
-	switch (factoryIndex) {
-     case 0:
-       return new Language(instanceURI);
-
-	}
+	public Resource createInstance(String classURI, String instanceURI, int factoryIndex) {
+		URL tableURL = getClass().getClassLoader().getResource(LanguageOntology.LANG_TABLE);
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(tableURL.openStream(), Charset.forName("UTF-8")));
+			int line = 0;
+			String ll = "";
+			while (line != factoryIndex + 1
+					&& (ll = br.readLine()) != null){
+				line++;
+			}
+			if (ll != null) {
+				String[] props =  ll.split("\\|");
+				if (props.length >= 4) {
+					return new LanguageImpl(props[1], props[2], props[3]);
+				}
+				else {
+					System.out.println(props.length);
+				}
+			}
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
 	return null;
   }
 }
--- ont.languages/src/main/java/org/universAAL/ontology/language/LanguageOntology.java
+++ ont.languages/src/main/java/org/universAAL/ontology/language/LanguageOntology.java
@@ -1,7 +1,11 @@
-
 package org.universAAL.ontology.language;
 
-import org.universAAL.middleware.owl.BoundingValueRestriction;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
 import org.universAAL.middleware.owl.DataRepOntology;
 import org.universAAL.middleware.owl.ManagedIndividual;
 import org.universAAL.middleware.owl.MergedRestriction;
@@ -9,87 +13,86 @@ import org.universAAL.middleware.owl.OntClassInfoSetup;
 import org.universAAL.middleware.owl.Ontology;
 import org.universAAL.middleware.rdf.Resource;
 import org.universAAL.middleware.rdf.TypeMapper;
-import org.universAAL.middleware.service.owl.ServiceBusOntology;
-import org.universAAL.ontology.location.LocationOntology;
-
-import org.universAAL.middleware.owl.ManagedIndividual;
-
-
-//import the factory for this ontology
 import org.universAAL.ontology.LanguageFactory;
-
 
 /**
  * @author AAL Studio: UML2Java transformation
  */
 public final class LanguageOntology extends Ontology {
-
-  private static LanguageFactory factory = new LanguageFactory();
-  public static final String NAMESPACE ="http://ontology.universAAL.org/LanguageOntology#";
 	
-  public LanguageOntology() {
-    super(NAMESPACE);
-  }
 
-  public void create() {
-    Resource r = getInfo();
-    r.setResourceComment("Ontology for languages based on ISO 639 codes (http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes)");
-    r.setResourceLabel("language");
-    addImport(DataRepOntology.NAMESPACE);
-    addImport(ServiceBusOntology.NAMESPACE);
-    addImport(LocationOntology.NAMESPACE);
-		
-    
+	
+	/**
+	 * 
+	 */
+	public static final String LANG_TABLE = "org/universAAL/ontology/impl/lang-table.dat";
+//	public static final String LANG_TABLE = "lang-table.dat";
+	private static LanguageFactory factory = new LanguageFactory();
+	public static final String NAMESPACE = "http://ontology.universAAL.org/LanguageOntology#";
 
+	public LanguageOntology() {
+		super(NAMESPACE);
+	}
 
-    // ******* Declaration of enumeration classes of the ontology ******* //
+	public void create() {
+		Resource r = getInfo();
+		r.setResourceComment("Ontology for languages based on ISO 639 codes (http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes)");
+		r.setResourceLabel("language");
+		addImport(DataRepOntology.NAMESPACE);
 
-    OntClassInfoSetup oci_NativeLabelType = createNewAbstractOntClassInfo(NativeLabelType.MY_URI);
-    OntClassInfoSetup oci_ISO639CodeType = createNewAbstractOntClassInfo(ISO639CodeType.MY_URI);
-    OntClassInfoSetup oci_EnglishLabelType = createNewAbstractOntClassInfo(EnglishLabelType.MY_URI);
+		// ******* Declaration of regular classes of the ontology ******* //
+		OntClassInfoSetup oci_Language = createNewAbstractOntClassInfo(Language.MY_URI);
 
+		// ******* Add content to regular classes of the ontology ******* //
+		oci_Language.setResourceComment("The concept for Language");
+		oci_Language.setResourceLabel("Language");
+		oci_Language.addSuperClass(ManagedIndividual.MY_URI);
+		oci_Language.addObjectProperty(Language.PROP_ENGLISH_LABEL)
+				.setFunctional();
+		oci_Language.addRestriction(MergedRestriction
+				.getAllValuesRestrictionWithCardinality(
+						Language.PROP_ENGLISH_LABEL,
+						TypeMapper.getDatatypeURI(String.class), 1, 1));
 
-    // ******* Declaration of regular classes of the ontology ******* //
-    OntClassInfoSetup oci_Language = createNewOntClassInfo(Language.MY_URI, factory, 0);
+		oci_Language.addObjectProperty(Language.PROP_ISO639CODE)
+				.setFunctional();
+		oci_Language.addRestriction(MergedRestriction
+				.getAllValuesRestrictionWithCardinality(
+						Language.PROP_ISO639CODE,
+						TypeMapper.getDatatypeURI(String.class), 1, 1));
 
+		oci_Language.addObjectProperty(Language.PROP_NATIVE_LABEL)
+				.setFunctional();
+		oci_Language.addRestriction(MergedRestriction
+				.getAllValuesRestrictionWithCardinality(
+						Language.PROP_NATIVE_LABEL,
+						TypeMapper.getDatatypeURI(String.class), 1, 1));
 
-    // ******* Add content to enumeration classes of the ontology ******* //
+		// ******* LOAD ALL Languages ******** //
 
-    oci_NativeLabelType.setResourceComment("");
-    oci_NativeLabelType.setResourceLabel("NativeLabelType");
-    oci_NativeLabelType.toEnumeration(new ManagedIndividual[] {
-       NativeLabelType.??????, NativeLabelType.Afaraf, NativeLabelType.Afrikaans, NativeLabelType.Akan, NativeLabelType.gjuha shqipe, NativeLabelType.????, NativeLabelType.??????? });
+		URL tableURL = getClass().getClassLoader().getResource(LANG_TABLE);
+		if (tableURL != null){
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					tableURL.openStream(), Charset.forName("UTF-8")));
+			int line = 0;
+			String ll;
+			while ((ll = br.readLine()) != null) {
+				if (line != 0) {
+					String name = ll.split("\\|")[1];
+					name = name.split(",")[0];
+					name = name.split(" ")[0];
+					OntClassInfoSetup lang = createNewOntClassInfo(NAMESPACE
+							+ name.toUpperCase(), factory);
+					System.out.println(name);
+					// can add value restrictions
+				}
+				line++;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}}
 
-    oci_ISO639CodeType.setResourceComment("");
-    oci_ISO639CodeType.setResourceLabel("ISO639CodeType");
-    oci_ISO639CodeType.toEnumeration(new ManagedIndividual[] {
-       ISO639CodeType.ab, ISO639CodeType.aa, ISO639CodeType.af, ISO639CodeType.ak, ISO639CodeType.sq, ISO639CodeType.am, ISO639CodeType.ar });
-
-    oci_EnglishLabelType.setResourceComment("");
-    oci_EnglishLabelType.setResourceLabel("EnglishLabelType");
-    oci_EnglishLabelType.toEnumeration(new ManagedIndividual[] {
-       EnglishLabelType.Abkhaz, EnglishLabelType.Afar, EnglishLabelType.Afrikaans, EnglishLabelType.Akan, EnglishLabelType.Albanian, EnglishLabelType.Amharic, EnglishLabelType.Arabic });
-
-
-
-    // ******* Add content to regular classes of the ontology ******* //
-    oci_Language.setResourceComment("");
-    oci_Language.setResourceLabel("Language");
-    oci_Language.addSuperClass(ManagedIndividual.MY_URI); 
-    oci_Language.addObjectProperty(Language.PROP_ENGLISH_LABEL).setFunctional();
-    oci_Language.addRestriction(MergedRestriction
-      .getAllValuesRestrictionWithCardinality(Language.PROP_ENGLISH_LABEL, 
-      EnglishLabelType.MY_URI, 1, 1));
-    
-    oci_Language.addObjectProperty(Language.PROP_ISO639CODE).setFunctional();
-    oci_Language.addRestriction(MergedRestriction
-      .getAllValuesRestrictionWithCardinality(Language.PROP_ISO639CODE, 
-      ISO639CodeType.MY_URI, 1, 1));
-    
-    oci_Language.addObjectProperty(Language.PROP_NATIVE_LABEL).setFunctional();
-    oci_Language.addRestriction(MergedRestriction
-      .getAllValuesRestrictionWithCardinality(Language.PROP_NATIVE_LABEL, 
-      NativeLabelType.MY_URI, 1, 1));
-    
-  }
+	}
 }
