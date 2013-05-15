7ea026b150579dc685a1c0c411a48cfc7661ebc6
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-05-15 19:33:43 +0000

    Automatic tag of ont.pom version: 1.3.2-SNAPSHOT

---
+++ ont.pom-1.3.2-SNAPSHOT/update.bat
@@ -0,0 +1,3 @@
+call mvn org.universAAL.support:uaalDirectives-maven-plugin:1.2.2-SNAPSHOT:modules-check -Ddirective.fix
+call mvn org.universAAL.support:uaalDirectives-maven-plugin:1.2.2-SNAPSHOT:dependency-check -Ddirective.fix
+pause
