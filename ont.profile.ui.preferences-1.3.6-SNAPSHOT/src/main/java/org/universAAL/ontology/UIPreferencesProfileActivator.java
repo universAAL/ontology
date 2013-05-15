cec9d61481390bac411fa827fcff798cc252f340
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-05-15 19:38:00 +0000

    Automatic tag of ont.profile.ui.preferences version: 1.3.6-SNAPSHOT

---
+++ ont.profile.ui.preferences-1.3.6-SNAPSHOT/src/main/java/org/universAAL/ontology/UIPreferencesProfileActivator.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright 2013 Ericsson Nikola Tesla d.d.
+ *
+ * Licensed under both Apache License, Version 2.0 and MIT License.
+ *
+ * See the NOTICE file distributed with this work for additional 
+ * information regarding copyright ownership
+ *	
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology;
+
+import org.universAAL.middleware.container.ModuleActivator;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.owl.OntologyManagement;
+import org.universAAL.ontology.ui.preferences.UIPreferencesProfileOntology;
+
+/**
+ * @author eandgrg
+ * 
+ */
+public class UIPreferencesProfileActivator implements ModuleActivator {
+
+    public static ModuleContext context;
+
+    UIPreferencesProfileOntology uipreferencesontology = new UIPreferencesProfileOntology();
+
+    public void start(ModuleContext mc) throws Exception {
+	UIPreferencesProfileActivator.context = mc;
+	OntologyManagement.getInstance().register(mc, uipreferencesontology);
+
+    }
+
+    public void stop(ModuleContext mc) throws Exception {
+
+	OntologyManagement.getInstance().unregister(mc, uipreferencesontology);
+
+    }
+
+}
