624611e1166f3c69634978bb0ccd19e5ae874937
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-05-15 19:34:14 +0000

    Automatic tag of ont.aalfficiency version: 1.3.2-SNAPSHOT

---
+++ ont.aalfficiency-1.3.2-SNAPSHOT/src/main/java/org/universAAL/ontology/aalfficiency/Aalfficiency.java
@@ -0,0 +1,29 @@
+package org.universAAL.ontology.aalfficiency;
+
+import org.universAAL.middleware.service.owl.Service;
+
+public class Aalfficiency extends Service {
+
+    public static final String MY_URI = AalfficiencyOntology.NAMESPACE
+	    + "Aalfficiency";
+
+    public static final String PROP_HAS_ELECTRICITY_DATA = AalfficiencyOntology.NAMESPACE
+	    + "hasElectricity";
+    public static final String PROP_HAS_ACTIVITY_DATA = AalfficiencyOntology.NAMESPACE
+	    + "hasActivity";
+    public static final String PROP_HAS_ADVICES = AalfficiencyOntology.NAMESPACE
+	    + "hasAdvices";
+
+    public Aalfficiency(String instanceURI) {
+	super(instanceURI);
+    }
+
+    public Aalfficiency() {
+	super();
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+}
