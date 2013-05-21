38d797615f4c254d949954ee873d351ce8e48035
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-05-21 12:38:15 +0000

    updated script.

--- ont.pom/update.bat
+++ ont.pom/update.bat
@@ -1,3 +1,2 @@
-call mvn org.universAAL.support:uaalDirectives-maven-plugin:1.2.2-SNAPSHOT:modules-check -Ddirective.fix
-call mvn org.universAAL.support:uaalDirectives-maven-plugin:1.2.2-SNAPSHOT:dependency-check -Ddirective.fix
+call mvn org.universAAL.support:uaalDirectives-maven-plugin:1.3.3-SNAPSHOT:update-root-children -Ddirective.fix
 pause
