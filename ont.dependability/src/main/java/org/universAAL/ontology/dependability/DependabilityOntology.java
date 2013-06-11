7d3d02d98267cc7fb7b39edbc047befdce91ff50
ABM Rubaiyat Sadat <abu.sadat@uni-siegen.de>
2013-06-11 12:34:19 +0000

    license header fixed

--- ont.dependability/src/main/java/org/universAAL/ontology/dependability/DependabilityOntology.java
+++ ont.dependability/src/main/java/org/universAAL/ontology/dependability/DependabilityOntology.java
@@ -1,6 +1,6 @@
 package org.universAAL.ontology.dependability;
 
-/**Copyright [2011-2012] [University of Siegen, Embedded System Instiute]
+/**Copyright [2011-2014] [University of Siegen, Embedded System Instiute]
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
