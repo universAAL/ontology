792c1ac897b2491aafe9b79a28ef23033fd23ca6
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-06-21 12:08:28 +0000

    Automatic tag of ont.multimedia version: 1.3.3-SNAPSHOT

---
+++ ont.multimedia-1.3.3-SNAPSHOT/src/main/java/org/universAAL/ontology/MultimediaFactory.java
@@ -0,0 +1,46 @@
+/*	Copyright 2008-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+
+	See the NOTICE file distributed with this work for additional
+	information regarding copyright ownership
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	  http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ontology;
+
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.rdf.ResourceFactory;
+import org.universAAL.ontology.multimedia.TV;
+import org.universAAL.ontology.multimedia.Stereoset;
+
+/**
+ * The factory for instantiating objects of the ontology classes.
+ * 
+ * @author Carsten Stockloew
+ */
+
+public class MultimediaFactory implements ResourceFactory {
+
+    public Resource createInstance(String classURI, String instanceURI,
+	    int factoryIndex) {
+
+	switch (factoryIndex) {
+	case 0:
+	    return new TV(instanceURI);
+	case 1:
+	    return new Stereoset(instanceURI);
+	}
+
+	return null;
+    }
+}
