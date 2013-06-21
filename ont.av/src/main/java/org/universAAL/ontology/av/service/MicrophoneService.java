36b8f8bf78337004f899ae3123d62fdc29d5c0b2
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-06-21 14:12:43 +0000

    Automatic tag of ont.av version: 2.0.0

---
+++ ont.av/src/main/java/org/universAAL/ontology/av/service/MicrophoneService.java
@@ -0,0 +1,58 @@
+/*
+	Copyright 2008-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+
+package org.universAAL.ontology.av.service;
+
+import org.universAAL.middleware.service.owl.Service;
+import org.universAAL.ontology.av.AVOntology;
+
+/**
+ * Ontological service that handles microphones. Methods included in this class
+ * are the mandatory ones for representing an ontological service in Java
+ * classes for uAAL.
+ * 
+ * @author climberg
+ * @author Carsten Stockloew
+ */
+public class MicrophoneService extends Service {
+
+    // customizing necessary!!!!
+    public static final String MY_URI = AVOntology.NAMESPACE + "MicrophoneService";
+    public static final String PROP_CONTROLS = AVOntology.NAMESPACE + "controls";
+    public static final String PROP_DELIVERS = AVOntology.NAMESPACE + "delivers";
+
+    /** default constructor */
+    public MicrophoneService() {
+	super();
+    }
+
+    public MicrophoneService(String uri) {
+	super(uri);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    // perhaps other settings
+    public int getPropSerializationType(String propURI) {
+	return PROP_SERIALIZATION_FULL;
+    }
+}
