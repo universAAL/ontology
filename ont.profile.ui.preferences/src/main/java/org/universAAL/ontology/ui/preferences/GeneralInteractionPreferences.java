b92414f3958f815d03a4e157036dcf1cd4a4672c
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-06-21 14:13:33 +0000

    Automatic tag of ont.profile.ui.preferences version: 2.0.0

---
+++ ont.profile.ui.preferences/src/main/java/org/universAAL/ontology/ui/preferences/GeneralInteractionPreferences.java
@@ -0,0 +1,114 @@
+/*******************************************************************************
+ * Copyright 2013 Ericsson Nikola Tesla d.d.
+ *
+ * Licensed under both Apache License, Version 2.0 and MIT License.
+ *
+ * See the NOTICE file distributed with this work for additional 
+ * information regarding copyright ownership
+ *	
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology.ui.preferences;
+
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.ui.owl.Modality;
+import org.universAAL.middleware.ui.owl.Preference;
+import org.universAAL.ontology.language.Language;
+
+public class GeneralInteractionPreferences extends Preference {
+    public static final String MY_URI = UIPreferencesProfileOntology.NAMESPACE
+	    + "GeneralInteractionPreferences";
+    public static final String PROP_SECONDARY_LANGUAGE = UIPreferencesProfileOntology.NAMESPACE
+	    + "secondaryLanguage";
+    public static final String PROP_PREFERRED_MODALITY = UIPreferencesProfileOntology.NAMESPACE
+	    + "preferredModality";
+    public static final String PROP_CONTENT_DENSITY = UIPreferencesProfileOntology.NAMESPACE
+	    + "contentDensity";
+    public static final String PROP_SECONDARY_MODALITY = UIPreferencesProfileOntology.NAMESPACE
+	    + "secondaryModality";
+    public static final String PROP_PREFERRED_LANGUAGE = UIPreferencesProfileOntology.NAMESPACE
+	    + "preferredLanguage";
+
+    public GeneralInteractionPreferences() {
+	super();
+    }
+
+    public GeneralInteractionPreferences(String uri) {
+	super(uri);
+    }
+
+    /** @see org.universAAL.middleware.owl.ManagedIndividual#getClassURI() */
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    /**
+     * @see org.universAAL.middleware.owl.ManagedIndividual#getPropSerializationType
+     *      (java.lang.String)
+     */
+    public int getPropSerializationType(String arg0) {
+	return Resource.PROP_SERIALIZATION_FULL;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.universAAL.middleware.owl.ManagedIndividual#isWellFormed()
+     */
+    public boolean isWellFormed() {
+	return true && hasProperty(PROP_SECONDARY_LANGUAGE)
+		&& hasProperty(PROP_PREFERRED_MODALITY)
+		&& hasProperty(PROP_CONTENT_DENSITY)
+		&& hasProperty(PROP_SECONDARY_MODALITY)
+		&& hasProperty(PROP_PREFERRED_LANGUAGE);
+    }
+
+    public Modality getPreferredModality() {
+	return (Modality) getProperty(PROP_PREFERRED_MODALITY);
+    }
+
+    public void setPreferredModality(Modality newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_PREFERRED_MODALITY, newPropValue);
+    }
+
+    public Modality getSecondaryModality() {
+	return (Modality) getProperty(PROP_SECONDARY_MODALITY);
+    }
+
+    public void setSecondaryModality(Modality newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_SECONDARY_MODALITY, newPropValue);
+    }
+
+    public Language getPreferredLanguage() {
+	return (Language) getProperty(PROP_PREFERRED_LANGUAGE);
+    }
+
+    public void setPreferredLanguage(Language newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_PREFERRED_LANGUAGE, newPropValue);
+    }
+
+    public Language getSecondaryLanguage() {
+	return (Language) getProperty(PROP_SECONDARY_LANGUAGE);
+    }
+
+    public void setSecondaryLanguage(Language newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_SECONDARY_LANGUAGE, newPropValue);
+    }
+
+    public ContentDensityType getContentDensity() {
+	return (ContentDensityType) getProperty(PROP_CONTENT_DENSITY);
+    }
+
+    public void setContentDensity(ContentDensityType newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_CONTENT_DENSITY, newPropValue);
+    }
+}
