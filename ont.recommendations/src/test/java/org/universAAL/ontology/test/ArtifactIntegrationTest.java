a2d66d693a24721bab563f3f9c3e915334e0e1c3
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-09-23 21:07:59 +0000

    test 4 bundle#2.

--- ont.recommendations/src/test/java/org/universAAL/ontology/test/ArtifactIntegrationTest.java
+++ ont.recommendations/src/test/java/org/universAAL/ontology/test/ArtifactIntegrationTest.java
@@ -1,5 +1,7 @@
 package org.universAAL.ontology.test;
 
+import java.util.Dictionary;
+
 import org.universAAL.itests.IntegrationTest;
 
 /**
@@ -9,6 +11,12 @@ import org.universAAL.itests.IntegrationTest;
 public class ArtifactIntegrationTest extends IntegrationTest {
 
     public void testComposite() {
+    	
+    	Dictionary d = bundleContext.getBundle(2).getHeaders();
+    	while (d.keys().hasMoreElements()) {
+			Object k = (Object) d.keys().nextElement();
+			System.out.println(k.toString() + " : " + d.get(k));
+		}
 	logAllBundles();
     }
 
