00bd4f7c35925838eb1207ce180d453d935bc0e8
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-10-02 13:54:27 +0000

    Automatic tag of ont.languages version: 2.0.1-SNAPSHOT

---
+++ ont.languages-2.0.1-SNAPSHOT/src/main/java/org/universAAL/ontology/LanguageActivator.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright 2013 Universidad Politécnica de Madrid
+ * Copyright 2013 Ericsson Nikola Tesla d.d.
+ *
+ * See the NOTICE file distributed with this work for additional 
+ * information regarding copyright ownership
+ 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *	
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology;
+
+import org.universAAL.middleware.container.ModuleActivator;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.owl.OntologyManagement;
+import org.universAAL.ontology.language.LanguageOntology;
+
+/**
+ * @author amedrano
+ * @author eandgrg
+ * 
+ */
+public class LanguageActivator implements ModuleActivator {
+
+    private LanguageOntology ont = new LanguageOntology();
+    public static ModuleContext context = null;
+
+    /** {@inheritDoc} */
+    public void start(ModuleContext mc) throws Exception {
+	LanguageActivator.context = mc;
+	OntologyManagement.getInstance().register(mc, ont);
+    }
+
+    /** {@inheritDoc} */
+    public void stop(ModuleContext mc) throws Exception {
+	OntologyManagement.getInstance().unregister(mc, ont);
+    }
+}
