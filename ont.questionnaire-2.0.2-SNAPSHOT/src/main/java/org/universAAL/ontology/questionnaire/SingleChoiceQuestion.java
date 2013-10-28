1eed46712240269ee79c817d67c10b01a3996508
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-10-28 10:29:43 +0000

    Automatic tag of ont.questionnaire version: 2.0.2-SNAPSHOT

---
+++ ont.questionnaire-2.0.2-SNAPSHOT/src/main/java/org/universAAL/ontology/questionnaire/SingleChoiceQuestion.java
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * Copyright 2012 UPM, http://www.upm.es 
+ Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology.questionnaire;
+
+/**
+ * This class describes the concept of SingleChoiceQuestion, its properties and
+ * its associated methods. A single-choice question is the kind of question that
+ * only has one choice as an answer.
+ * 
+ * @author mdelafuente
+ * 
+ */
+public class SingleChoiceQuestion extends Question {
+    public static final String MY_URI = QuestionnaireOntology.NAMESPACE
+	    + "SingleChoiceQuestion";
+
+    public SingleChoiceQuestion() {
+	super();
+    }
+
+    public SingleChoiceQuestion(String uri) {
+	super(uri);
+    }
+
+    /**
+     * A SingleChoiceQuestion can be implemented with the question wording and
+     * the answer type if there is not a correct answer for the question.
+     * 
+     * @param qWording
+     * @param hasCorrectAnswer
+     * @param answerType
+     * @see Question
+     */
+
+    public SingleChoiceQuestion(String qWording, String answerType) {
+	super(qWording, answerType);
+    }
+
+    /**
+     * A SingleChoiceQuestion can be implemented with the question wording, the
+     * answer type and the possible choices, when a correct answer is not
+     * specified
+     * 
+     * @param qWording
+     * @param answerType
+     * @param choices
+     * @see Question
+     */
+    public SingleChoiceQuestion(String qWording, String answerType,
+	    ChoiceLabel[] choices) {
+	super(qWording, answerType, choices);
+    }
+
+    /**
+     * A SingleChoiceQuestion can be implemented with the question wording, the
+     * answer type, and the possible choices, when a correct answer is given.
+     * 
+     * @param qWording
+     * @param hasCorrectAnswer
+     * @param correctAnswer
+     * @param answerType
+     * @param choices
+     * @see Question
+     */
+    public SingleChoiceQuestion(String qWording, Object correctAnswer,
+	    String answerType, ChoiceLabel[] choices) {
+	super(qWording, correctAnswer, answerType, choices);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public int getPropSerializationType(String arg0) {
+	return PROP_SERIALIZATION_FULL;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+}
---
+++ ont.questionnaire-2.0.2-SNAPSHOT/src/test/java/org/universAAL/ennumerations/ontology/owl/PartsOfDay.java
@@ -0,0 +1,77 @@
+package org.universAAL.ennumerations.ontology.owl;
+
+import org.universAAL.middleware.owl.ManagedIndividual;
+import org.universAAL.ontology.questionnaire.QuestionnaireOntology;
+
+public class PartsOfDay extends ManagedIndividual {
+
+    public static final String MY_URI = QuestionnaireOntology.NAMESPACE
+	    + "PartsOfDay";
+    public static final int IN_THE_MORNING = 0;
+    public static final int IN_THE_AFTERNOON = 1;
+    public static final int IN_THE_EVENING = 2;
+
+    private static final String[] names = { "inTheMorning", "inTheAfternoon",
+	    "inTheEvening" };
+
+    public static final PartsOfDay inTheMorning = new PartsOfDay(IN_THE_MORNING);
+    public static final PartsOfDay inTheAfternoon = new PartsOfDay(
+	    IN_THE_AFTERNOON);
+    public static final PartsOfDay inTheEvening = new PartsOfDay(IN_THE_EVENING);
+
+    private int order;
+
+    private PartsOfDay(int order) {
+
+	super(QuestionnaireOntology.NAMESPACE + names[order]);
+	this.order = order;
+    }
+
+    public int getPropSerializationType(String propURI) {
+	return PROP_SERIALIZATION_OPTIONAL;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+
+    public String name() {
+	return names[order];
+    }
+
+    public int ord() {
+	return order;
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public static PartsOfDay getPartsOfDayByOrder(int order) {
+	switch (order) {
+	case IN_THE_MORNING:
+	    return inTheMorning;
+	case IN_THE_AFTERNOON:
+	    return inTheAfternoon;
+	case IN_THE_EVENING:
+	    return inTheEvening;
+	default:
+	    return null;
+	}
+    }
+
+    public static final PartsOfDay valueOf(String name) {
+	if (name == null)
+	    return null;
+
+	if (name.startsWith(QuestionnaireOntology.NAMESPACE))
+	    name = name.substring(QuestionnaireOntology.NAMESPACE.length());
+
+	for (int i = IN_THE_MORNING; i <= IN_THE_EVENING; i++)
+	    if (names[i].equals(name))
+		return getPartsOfDayByOrder(i);
+
+	return null;
+    }
+
+}
