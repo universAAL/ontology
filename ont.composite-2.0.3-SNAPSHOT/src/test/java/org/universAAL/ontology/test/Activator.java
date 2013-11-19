fcf414f9152956f89b0aeee61231b3d8313d9906
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-11-19 13:50:11 +0000

    Automatic tag of ont.composite version: 2.0.3-SNAPSHOT

---
+++ ont.composite-2.0.3-SNAPSHOT/src/test/java/org/universAAL/ontology/test/Activator.java
@@ -0,0 +1,28 @@
+package org.universAAL.ontology.test;
+
+import org.universAAL.middleware.container.ModuleActivator;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.owl.OntologyManagement;
+
+public class Activator implements ModuleActivator {
+    
+    public static ModuleContext mc;
+
+    public void start(ModuleContext mc) throws Exception {
+	Activator.mc = mc;
+	
+	OntologyManagement om = OntologyManagement.getInstance();
+	String uris[] = om.getOntoloyURIs();
+
+	for (int i = 0; i < uris.length; i++) {
+	    System.out.println("-- Serializing ontology: " + i + ":\t"
+		    + uris[i]);
+	    OwlOWLCreator.doit(om.getOntology(uris[i]));
+	}
+	
+	System.out.println("--DONE--");
+    }
+
+    public void stop(ModuleContext mc) throws Exception {
+    }
+}
