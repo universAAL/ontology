86827f05c6186b70d425c4aee0a2028defcaa5ff
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-01-20 14:53:50 +0000

    Automatic tag of ont.profile version: 3.0.0

---
+++ ont.profile/src/main/java/org/universAAL/ontology/vcard/Organization.java
@@ -0,0 +1,54 @@
+/*
+	Copyright 2008-2014 ITACA-TSB, http://www.tsb.upv.es
+	Instituto Tecnologico de Aplicaciones de Comunicacion 
+	Avanzadas - Grupo Tecnologias para la Salud y el 
+	Bienestar (TSB)
+	
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ontology.vcard;
+
+import org.universAAL.middleware.owl.ManagedIndividual;
+
+public class Organization extends ManagedIndividual {
+    public static final String MY_URI = VCardOntology.NAMESPACE
+	    + "Organization";
+    public static final String PROP_ORGANIZATION_NAME = VCardOntology.NAMESPACE
+	    + "organization-name";
+    public static final String PROP_ORGANIZATION_UNIT = VCardOntology.NAMESPACE
+	    + "organization-unit";
+
+    public Organization() {
+	super();
+    }
+
+    public Organization(String uri) {
+	super(uri);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public int getPropSerializationType(String propURI) {
+	return PROP_SERIALIZATION_FULL;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+}
