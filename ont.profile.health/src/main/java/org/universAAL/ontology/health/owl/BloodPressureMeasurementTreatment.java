fca7c835762568ff10810fc04c81ed239c3494a9
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-01-20 14:54:25 +0000

    Automatic tag of ont.profile.health version: 3.0.0

---
+++ ont.profile.health/src/main/java/org/universAAL/ontology/health/owl/BloodPressureMeasurementTreatment.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright 2012 UPM, http://www.upm.es - Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology.health.owl;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.universAAL.ontology.profile.AssistedPersonProfile;
+import org.universAAL.ontology.profile.Caregiver;
+
+public class BloodPressureMeasurementTreatment extends TakeMeasurementActivity {
+
+    public static final String MY_URI = HealthProfileOntology.NAMESPACE
+	    + "BloodPreasureMeasurementTreatment";
+
+    public BloodPressureMeasurementTreatment() {
+	super();
+    }
+
+    public BloodPressureMeasurementTreatment(String uri) {
+	super(uri);
+    }
+
+    public BloodPressureMeasurementTreatment(AssistedPersonProfile assistedPerson,
+	    Caregiver caregiver, String tname, String description,
+	    XMLGregorianCalendar stDt, String diseaseURI, BloodPressureRequirement wr) {
+	super(assistedPerson, caregiver, tname, description, stDt, diseaseURI);
+	this.setMeasurementRequirements(wr);
+    }
+
+    public BloodPressureMeasurementTreatment(AssistedPersonProfile assistedPerson,
+	    Caregiver caregiver, String tname, TreatmentPlanning tp,
+	    String description, String diseaseURI, BloodPressureRequirement wr) {
+	super(assistedPerson, caregiver, tname, tp, description, diseaseURI);
+	this.setMeasurementRequirements(wr);
+    }
+
+    public BloodPressureMeasurementTreatment(String tname, String description,
+	    String diseaseURI, BloodPressureRequirement wr) {
+	super(tname, description, diseaseURI);
+	this.setMeasurementRequirements(wr);
+    }
+
+    public BloodPressureMeasurementTreatment(String tname, String description,
+	    XMLGregorianCalendar stDt, String diseaseURI, BloodPressureRequirement wr) {
+	super(tname, description, stDt, diseaseURI);
+	this.setMeasurementRequirements(wr);
+    }
+
+    public BloodPressureMeasurementTreatment(String tname, String description,
+	    TreatmentPlanning tp, String diseaseURI, BloodPressureRequirement wr) {
+	super(tname, description, tp, diseaseURI);
+	this.setMeasurementRequirements(wr);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public int getPropSerializationType(String arg0) {
+	return PROP_SERIALIZATION_FULL;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+
+}
---
+++ ont.profile.health/src/main/java/org/universAAL/ontology/health/owl/services/PlannedSessionManagementService.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright 2012 UPM, http://www.upm.es 
+ Universidad Politénica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package org.universAAL.ontology.health.owl.services;
+
+import org.universAAL.ontology.health.owl.HealthProfileOntology;
+
+/**
+ * This class describes the planned session management service, which consists
+ * of editing, creating and deleting a planned session. Also, this service lists
+ * the planned sessions that composes a treatment planning.
+ * 
+ * @author mdelafuente
+ * 
+ */
+public class PlannedSessionManagementService extends HealthService {
+
+    // NAMESPACE & PROPERTIES
+    public static final String MY_URI = HealthProfileOntology.NAMESPACE
+	    + "SessionManagementService";
+
+    public static final String PROP_MANAGES_SESSION = HealthProfileOntology.NAMESPACE
+	    + "managesSession";
+
+    public static final String PROP_ASSOCIATED_TREATMENT = HealthProfileOntology.NAMESPACE
+	    + "plannedSessionsTreatment";
+
+    // CONSTRUCTORS
+    public PlannedSessionManagementService() {
+	super();
+    }
+
+    public PlannedSessionManagementService(String uri) {
+	super(uri);
+    }
+
+    /** {@inheritDoc} */
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    /** {@inheritDoc} */
+    public int getPropSerializationType(String propURI) {
+	return PROP_MANAGES_SESSION.equals(propURI) ? PROP_SERIALIZATION_FULL
+		: super.getPropSerializationType(propURI);
+    }
+
+    /** {@inheritDoc} */
+    public boolean isWellFormed() {
+	return true;
+    }
+
+}
