9d8cb67db2f442488048b4dc637803b956d7c4e2
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-01-20 14:41:59 +0000

    Automatic tag of ont.pom version: 2.0.4-SNAPSHOT

---
+++ ont.pom-2.0.4-SNAPSHOT/composite/src/test/java/org/universAAL/ontology/test/ArtifactIT.java
@@ -0,0 +1,10 @@
+package org.universAAL.ontology.test;
+
+import org.universAAL.itests.IntegrationTest;
+
+public class ArtifactIT extends IntegrationTest {
+
+    public void testComposite() {
+	logAllBundles();
+    }
+}
