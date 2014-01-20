f3d4a3b9a34f0e694ae8d990fa23f4bca312c555
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-01-20 14:54:32 +0000

    Automatic tag of ont.situation.reasoner version: 3.0.0

---
+++ ont.situation.reasoner/src/main/java/org/universAAL/ontology/ReasoningFactory.java
@@ -0,0 +1,48 @@
+/*	
+	Copyright 2008-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ontology;
+
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.rdf.ResourceFactory;
+import org.universAAL.ontology.reasoner.Query;
+import org.universAAL.ontology.reasoner.ReasoningService;
+import org.universAAL.ontology.reasoner.Rule;
+import org.universAAL.ontology.reasoner.Situation;
+
+
+public class ReasoningFactory implements ResourceFactory {
+
+    public Resource createInstance(String classURI, String instanceURI,
+	    int factoryIndex) {
+
+	switch (factoryIndex) {
+	case 0:
+	    return new Situation(instanceURI);
+	case 1:
+	    return new Query(instanceURI);
+	case 2:
+	    return new Rule(instanceURI);
+	case 3:
+	    return new ReasoningService(instanceURI);
+
+	}
+	return null;
+    }
+}
