e0465b0c42feab27d457dc46eacd1a3434585ec7
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-01-20 14:54:01 +0000

    Automatic tag of ont.dependability version: 3.0.0

---
+++ ont.dependability/src/main/java/org/universAAL/ontology/dependability/Sensor.java
@@ -0,0 +1,60 @@
+package org.universAAL.ontology.dependability;
+/**Copyright [2011-2014] [University of Siegen, Embedded System Instiute]
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ * @author <a href="mailto:abu.sadat@uni-siegen.de">Rubaiyat Sadat</a>
+ *	       ©2012
+ */
+import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.universAAL.ontology.phThing.Device;
+
+public class Sensor extends Device {
+    public static final String MY_URI = DependabilityOntology.NAMESPACE
+	    + "Sensor";
+    public static final String PROP_SYMPTOM = DependabilityOntology.NAMESPACE
+	    + "symptom";
+
+    public Sensor() {
+	super();
+    }
+
+    public Sensor(String uri) {
+	super(uri);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public int getPropSerializationType(String arg0) {
+	// TODO Implement or if for Device subclasses: remove
+	return 0;
+    }
+
+    public boolean isWellFormed() {
+	return true && hasProperty(PROP_SYMPTOM);
+    }
+
+    public Symptom getSymptom() {
+	return (Symptom) getProperty(PROP_SYMPTOM);
+    }
+
+    public void setSymptom(Symptom newPropValue) {
+	if (newPropValue != null)
+	    changeProperty(PROP_SYMPTOM, newPropValue);
+    }
+}
