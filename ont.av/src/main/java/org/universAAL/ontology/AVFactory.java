5389ebce2d57eb977489e9ab4146140e9343964b
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-01-20 14:53:56 +0000

    Automatic tag of ont.av version: 3.0.0

---
+++ ont.av/src/main/java/org/universAAL/ontology/AVFactory.java
@@ -0,0 +1,76 @@
+/*
+	Copyright 2008-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ontology;
+
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.rdf.ResourceFactory;
+import org.universAAL.ontology.av.device.LoudSpeaker;
+import org.universAAL.ontology.av.device.Microphone;
+import org.universAAL.ontology.av.device.VideoCamera;
+import org.universAAL.ontology.av.service.CameraService;
+import org.universAAL.ontology.av.service.LoudSpeakerService;
+import org.universAAL.ontology.av.service.MicrophoneService;
+import org.universAAL.ontology.av.streaming.AudioFile;
+import org.universAAL.ontology.av.streaming.AudioFormat;
+import org.universAAL.ontology.av.streaming.AudioStream;
+import org.universAAL.ontology.av.streaming.EndPoint;
+import org.universAAL.ontology.av.streaming.VideoFormat;
+import org.universAAL.ontology.av.streaming.VideoStream;
+
+/**
+ * 
+ * @author Carsten Stockloew
+ * 
+ */
+public class AVFactory implements ResourceFactory {
+
+    public Resource createInstance(String classURI, String instanceURI,
+	    int factoryIndex) {
+
+	switch (factoryIndex) {
+	case 0:
+	    return new LoudSpeaker(instanceURI);
+	case 1:
+	    return new Microphone(instanceURI);
+	case 2:
+	    return new AudioFormat(instanceURI);
+	case 3:
+	    return new VideoFormat(instanceURI);
+	case 4:
+	    return new EndPoint();
+	case 5:
+	    return new AudioFile(instanceURI);
+	case 6:
+	    return new AudioStream(instanceURI);
+	case 7:
+	    return new VideoStream(instanceURI);
+	case 8:
+	    return new VideoCamera(instanceURI);
+	case 9:
+	    return new CameraService(instanceURI);
+	case 10:
+	    return new LoudSpeakerService(instanceURI);
+	case 11:
+	    return new MicrophoneService(instanceURI);
+	}
+
+	return null;
+    }
+}
---
+++ ont.av/src/main/java/org/universAAL/ontology/av/device/Microphone.java
@@ -0,0 +1,87 @@
+/*
+	Copyright 2008-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+
+package org.universAAL.ontology.av.device;
+
+import org.universAAL.middleware.util.Constants;
+import org.universAAL.ontology.av.AVOntology;
+import org.universAAL.ontology.phThing.Device;
+
+/**
+ * Ontological representation of a microphone. Methods included in this class
+ * are the mandatory ones for representing an ontological concept in Java
+ * classes for uAAL. Usually it includes getters and setters for most of its
+ * properties.
+ * 
+ * @author climberg
+ * @author Carsten Stockloew
+ */
+public class Microphone extends Device {
+
+    public static final String MY_URI = AVOntology.NAMESPACE + "Microphone";
+    public static final String PROP_VOLUME = AVOntology.NAMESPACE + "volume";
+    public static final String PROP_AMPLIFICATION = AVOntology.NAMESPACE + "amplification";
+    public static final String PROP_BANDWIDTH_IN_HZ = AVOntology.NAMESPACE + "bandwidthInHz";
+    public static final String PROP_SAMPLINGRATE_IN_HZ = AVOntology.NAMESPACE + "samplingRateInHz";
+    public static final String PROP_AUDIO_COMPRESSION = AVOntology.NAMESPACE + "audioCompression";
+    public static final String PROP_POINTS_TO = AVOntology.NAMESPACE + "pointsTo";
+    public static final String PROP_ACTIVITY = AVOntology.NAMESPACE + "activity";
+    public static final String PROP_SILENCE_LEVEL = AVOntology.NAMESPACE + "silenceLevel";
+    public static final String PROP_USE_ECHO_SUPRESSION = AVOntology.NAMESPACE + "useEchoSupression";
+    public static final String PROP_IS_MUTED = AVOntology.NAMESPACE + "isMuted";
+
+    /** The default constructor */
+    public Microphone() {
+	super();
+    }
+
+    public Microphone(String uri) {
+	super((uri == null || uri.lastIndexOf('#') > 0) ? uri
+		: Constants.uAAL_MIDDLEWARE_LOCAL_ID_PREFIX + uri);
+    }
+
+    /**
+     * numProps is a dummy parameter this constructor is needed for constructing
+     * microphones with an unique URI
+     */
+    public Microphone(int numProps) {
+	// TODO: two different constructors are necessary. one for using unique
+	// URIs and one for using a special URI as parameter
+	super(AVOntology.NAMESPACE, numProps);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public int getPropSerializationType(String propURI) {
+	if (PROP_VOLUME.equals(propURI) || PROP_AMPLIFICATION.equals(propURI)
+		|| PROP_BANDWIDTH_IN_HZ.equals(propURI)
+		|| PROP_SAMPLINGRATE_IN_HZ.equals(propURI)
+		|| PROP_AUDIO_COMPRESSION.equals(propURI)
+		|| PROP_POINTS_TO.equals(propURI)
+		|| PROP_USE_ECHO_SUPRESSION.equals(propURI)
+		|| PROP_ACTIVITY.equals(propURI)
+		|| PROP_SILENCE_LEVEL.equals(propURI)
+		|| PROP_IS_MUTED.equals(propURI))
+	    return PROP_SERIALIZATION_FULL;
+	return super.getPropSerializationType(propURI);
+    }
+}
