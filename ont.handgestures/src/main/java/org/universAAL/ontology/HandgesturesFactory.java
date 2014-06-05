d186dcff23a57927c561ec643615da99da7eea6a
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-06-05 08:31:13 +0000

    Automatic tag of ont.handgestures version: 3.1.0

---
+++ ont.handgestures/src/main/java/org/universAAL/ontology/HandgesturesFactory.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright 2013 Ericsson Nikola Tesla d.d.
+ *
+ * See the NOTICE file distributed with this work for additional 
+ * information regarding copyright ownership
+ *	
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *	
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *	
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology;
+
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.rdf.ResourceFactory;
+import org.universAAL.ontology.handgestures.HandGestureService;
+import org.universAAL.ontology.handgestures.HandGestures;
+
+/**
+ * Ontology factory for serialization
+ * 
+ * @author eandgrg
+ * 
+ */
+public class HandgesturesFactory implements ResourceFactory {
+
+    public Resource createInstance(String classURI, String instanceURI,
+	    int factoryIndex) {
+
+	switch (factoryIndex) {
+	case 0:
+	    return new HandGestures(instanceURI);
+	case 1:
+	    return new HandGestureService(instanceURI);
+
+	}
+	return null;
+    }
+}
