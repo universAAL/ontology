185f6e3de13e7b0ce8f600af5018db09a23b7d43
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-06-05 08:31:20 +0000

    Automatic tag of ont.unit version: 3.1.0

---
+++ ont.unit/src/main/java/org/universAAL/ontology/unit/DerivedUnit.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright 2012 UPM - Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology.unit;
+
+public abstract class DerivedUnit extends Unit {
+    public static final String MY_URI = UnitOntology.NAMESPACE + "DerivedUnit";
+    public static final String PROP_BASE_UNIT = UnitOntology.NAMESPACE
+	    + "baseUnit";
+    public static final String PROP_BASE_PREFIX = UnitOntology.NAMESPACE
+	    + "basePrefix";
+
+    public DerivedUnit() {
+	super();
+    }
+
+    public DerivedUnit(String uri) {
+	super(uri);
+    }
+
+    public DerivedUnit(Unit baseUnit) {
+	super();
+	setBaseUnit(baseUnit);
+    }
+
+    public DerivedUnit(String uri, Unit baseUnit) {
+	super(uri);
+	setBaseUnit(baseUnit);
+    }
+
+    public DerivedUnit(String uri, Unit baseUnit, Prefix basePrefix) {
+	super(uri);
+	setBaseUnit(baseUnit);
+	setBasePrefix(basePrefix);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public int getPropSerializationType(String arg0) {
+	return super.getPropSerializationType(arg0);
+    }
+
+    public boolean isWellFormed() {
+	return true && hasProperty(PROP_BASE_UNIT);
+    }
+
+    public Unit getBaseUnit() {
+	return (Unit) getProperty(PROP_BASE_UNIT);
+    }
+
+    private void setBaseUnit(Unit newPropValue) {
+	if (newPropValue != null)
+	    props.put(PROP_BASE_UNIT, newPropValue);
+    }
+
+    public Prefix getBasePrefix() {
+	return (Prefix) getProperty(PROP_BASE_PREFIX);
+    }
+
+    private void setBasePrefix(Prefix newPropValue) {
+	if (newPropValue != null)
+	    props.put(PROP_BASE_PREFIX, newPropValue);
+    }
+}
---
+++ ont.unit/src/main/java/org/universAAL/ontology/unit/services/UnitService.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright 2012 UPM - Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology.unit.services;
+
+import org.universAAL.middleware.service.owl.Service;
+import org.universAAL.ontology.unit.UnitOntology;
+
+public class UnitService extends Service {
+    public static final String MY_URI = UnitOntology.NAMESPACE + "UnitService";
+    public static final String PROP_CONTROLS = UnitOntology.NAMESPACE
+	    + "controls";
+
+    public UnitService() {
+	super();
+    }
+
+    public UnitService(String uri) {
+	super(uri);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public int getPropSerializationType(String arg0) {
+	return PROP_SERIALIZATION_FULL;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+}
