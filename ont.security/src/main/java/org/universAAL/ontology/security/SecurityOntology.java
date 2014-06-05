4c44f5287b6e26d289e6d9572fda04e6493af827
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-06-05 08:32:24 +0000

    Automatic tag of ont.security version: 3.1.0

---
+++ ont.security/src/main/java/org/universAAL/ontology/security/SecurityOntology.java
@@ -0,0 +1,172 @@
+/*******************************************************************************
+ * Copyright 2011 Ericsson Nikola Tesla d.d.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology.security;
+
+import org.universAAL.middleware.owl.AllValuesFromRestriction;
+import org.universAAL.middleware.owl.DataRepOntology;
+import org.universAAL.middleware.owl.ManagedIndividual;
+import org.universAAL.middleware.owl.MergedRestriction;
+import org.universAAL.middleware.owl.OntClassInfoSetup;
+import org.universAAL.middleware.owl.Ontology;
+import org.universAAL.middleware.owl.supply.AbsLocation;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.rdf.TypeMapper;
+import org.universAAL.middleware.service.owl.Service;
+import org.universAAL.middleware.service.owl.ServiceBusOntology;
+import org.universAAL.middleware.xsd.Base64Binary;
+import org.universAAL.ontology.SecurityFactory;
+import org.universAAL.ontology.location.LocationOntology;
+import org.universAAL.ontology.phThing.Device;
+import org.universAAL.ontology.profile.ProfileOntology;
+import org.universAAL.ontology.profile.SubProfile;
+import org.universAAL.ontology.profile.User;
+
+/**
+ * @author eandgrg
+ * 
+ */
+public final class SecurityOntology extends Ontology {
+
+    private static SecurityFactory factory = new SecurityFactory();
+    public static final String NAMESPACE = "http://ontology.universAAL.org/Security.owl#";
+
+    public static final String PROP_SESSION = NAMESPACE + "hasSession";
+    public static final String PROP_AUTHENTICATED = NAMESPACE + "authenticatedTo";
+    public static final String PROP_REVOKED = NAMESPACE + "deauthenticatedBy";
+    
+
+    public SecurityOntology() {
+	super(NAMESPACE);
+    }
+
+    public void create() {
+	Resource r = getInfo();
+	r
+		.setResourceComment("The ontology defining the most general concepts dealing security.");
+	r.setResourceLabel("Security");
+	addImport(DataRepOntology.NAMESPACE);
+	addImport(ServiceBusOntology.NAMESPACE);
+	addImport(LocationOntology.NAMESPACE);
+	addImport(ProfileOntology.NAMESPACE);
+
+	OntClassInfoSetup oci_credentials = createNewAbstractOntClassInfo(Credentials.MY_URI);
+	OntClassInfoSetup oci_session = createNewAbstractOntClassInfo(Session.MY_URI);
+	
+	OntClassInfoSetup oci_userPass = createNewOntClassInfo(UserPasswordCredentials.MY_URI, factory, 0);
+	OntClassInfoSetup oci_devSession = createNewOntClassInfo(DeviceBoundSession.MY_URI, factory, 1);
+	OntClassInfoSetup oci_locSession = createNewOntClassInfo(LocationBoundSession.MY_URI, factory, 2);
+	OntClassInfoSetup oci_sessionService = createNewOntClassInfo(SessionManagementService.MY_URI, factory, 3);
+	OntClassInfoSetup oci_authService = createNewOntClassInfo(AuthenticationService.MY_URI, factory, 4);	
+	OntClassInfoSetup oci_secProf = createNewOntClassInfo(SecuritySubprofile.MY_URI, factory, 5);	
+	
+	// Credentials
+	oci_credentials.setResourceLabel("Credentials");
+	oci_credentials.setResourceComment("The abstract concept of the required object to authenticate anyone.");
+	oci_credentials.addSuperClass(ManagedIndividual.MY_URI);
+	
+	// Sesssion
+	oci_session.setResourceLabel("Session");
+	oci_session.setResourceComment("Session indicates the validity of an authentication period and or place.");
+	oci_session.addSuperClass(ManagedIndividual.MY_URI);
+	oci_session.addObjectProperty(Session.PROP_BOUND_TO);
+	oci_session.addInstance(Session.invalid);
+
+	// UserPass
+	oci_userPass.setResourceComment("Authentication by User password.");
+	oci_userPass.setResourceLabel("UserPassword");
+	oci_userPass.addSuperClass(Credentials.MY_URI);
+	oci_userPass.addDatatypeProperty(UserPasswordCredentials.PROP_USERNAME).setFunctional();
+	oci_userPass.addRestriction(MergedRestriction
+		.getAllValuesRestrictionWithCardinality(
+			UserPasswordCredentials.PROP_USERNAME, TypeMapper
+				.getDatatypeURI(String.class), 1, 1));
+
+	oci_userPass.addDatatypeProperty(UserPasswordCredentials.PROP_PASSWORD).setFunctional();
+	oci_userPass.addRestriction(MergedRestriction
+		.getAllValuesRestrictionWithCardinality(
+			UserPasswordCredentials.PROP_PASSWORD, TypeMapper
+				.getDatatypeURI(Base64Binary.class), 1, 1));
+	
+	oci_userPass.addDatatypeProperty(UserPasswordCredentials.PROP_PASSWORD_DIGEST).setFunctional();
+	oci_userPass.addRestriction(MergedRestriction
+		.getAllValuesRestrictionWithCardinality(
+			UserPasswordCredentials.PROP_PASSWORD_DIGEST, TypeMapper
+				.getDatatypeURI(String.class), 0, 1));
+	//XXX Password Digest should be an ontological class?
+	
+	// DeviceBoundSession
+	oci_devSession.setResourceComment("Session bounded to a single device.");
+	oci_devSession.setResourceLabel("Device Session");
+	oci_devSession.addSuperClass(Session.MY_URI);
+	oci_devSession.addObjectProperty(DeviceBoundSession.PROP_BOUNDED_DEVICE).addSuperProperty(Session.PROP_BOUND_TO);
+	oci_devSession.addRestriction(MergedRestriction.getAllValuesRestrictionWithCardinality(DeviceBoundSession.PROP_BOUNDED_DEVICE, Device.MY_URI, 1, 1));
+	
+	// LocationBoundSession
+	oci_locSession.setResourceComment("Session bounded to a single location.");
+	oci_locSession.setResourceLabel("Location Session");
+	oci_locSession.addSuperClass(Session.MY_URI);
+	oci_locSession.addObjectProperty(LocationBoundSession.PROP_BOUNDED_LOCATION).addSuperProperty(Session.PROP_BOUND_TO);
+	oci_locSession.addRestriction(MergedRestriction.getAllValuesRestrictionWithCardinality(LocationBoundSession.PROP_BOUNDED_LOCATION, AbsLocation.MY_URI, 1, 1));
+
+	// SecurityProfile
+	oci_secProf.setResourceComment("Profile storing credentials, Roles, AcessRights and any security concept for a given profilable.");
+	oci_secProf.setResourceLabel("Security Profile");
+	oci_secProf.addSuperClass(SubProfile.MY_URI);
+	oci_secProf.addObjectProperty(SecuritySubprofile.PROP_CREDENTIALS);
+	oci_secProf.addRestriction(MergedRestriction.getAllValuesRestriction(SecuritySubprofile.PROP_CREDENTIALS, Credentials.MY_URI));
+
+	
+	/*
+	 * Extensions provided
+	 */
+	// for user
+	OntClassInfoSetup oci_user = extendExistingOntClassInfo(User.MY_URI);
+	// USER.hasSession property
+	oci_user.addObjectProperty(PROP_SESSION);
+	oci_user.addRestriction(MergedRestriction.getAllValuesRestrictionWithCardinality(PROP_SESSION, Session.MY_URI, 0, 1));
+	//USER.authenticatedTo
+	oci_user.addObjectProperty(PROP_AUTHENTICATED);
+	oci_user.addRestriction(MergedRestriction.getAllValuesRestrictionWithCardinality(PROP_AUTHENTICATED, Device.MY_URI, 0, 1));
+	//USER.deauthenticatedby
+	oci_user.addObjectProperty(PROP_REVOKED);
+	oci_user.addRestriction(MergedRestriction.getAllValuesRestrictionWithCardinality(PROP_REVOKED, Device.MY_URI, 0, 1));
+	
+	
+	/*
+	 * Services
+	 */
+	//Authentication Service
+	oci_authService.setResourceComment("Service to Athenticate a User Given some credentials.");
+	oci_authService.setResourceLabel("Authentication Service");
+	oci_authService.addSuperClass(Service.MY_URI);
+	oci_authService.addObjectProperty(AuthenticationService.PROP_GIVEN_CREDENTIALS);
+	oci_authService.addRestriction(MergedRestriction.getAllValuesRestrictionWithCardinality(AuthenticationService.PROP_GIVEN_CREDENTIALS, Credentials.MY_URI, 1, 1));
+	oci_authService.addObjectProperty(AuthenticationService.PROP_AUTHENTICATED_USER);
+	oci_authService.addRestriction(MergedRestriction.getAllValuesRestrictionWithCardinality(AuthenticationService.PROP_AUTHENTICATED_USER, User.MY_URI, 0, 1));
+	
+	//Session Service
+	oci_sessionService.setResourceComment("Service to check the validity of sessions, or available sessions.");
+	oci_sessionService.setResourceLabel("Session Service");
+	oci_sessionService.addSuperClass(Service.MY_URI);
+	oci_sessionService.addObjectProperty(SessionManagementService.PROP_USER);
+	oci_sessionService.addRestriction(MergedRestriction.getAllValuesRestriction(SessionManagementService.PROP_USER, User.MY_URI));
+	//add restrinction on ppath {SessionManagementService.PROP_USER, User.PROP_HAS_SESSION}
+		MergedRestriction res = new MergedRestriction(SessionManagementService.PROP_USER);
+		res.addRestriction(new AllValuesFromRestriction(SessionManagementService.PROP_USER, 
+			new AllValuesFromRestriction(PROP_SESSION,
+				Session.MY_URI)));
+    }
+}
---
+++ ont.security/src/main/java/org/universAAL/ontology/security/SessionManagementService.java
@@ -0,0 +1,138 @@
+/*******************************************************************************
+ * Copyright 2013 Universidad Politécnica de Madrid
+ * Copyright 2013 Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package org.universAAL.ontology.security;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.service.CallStatus;
+import org.universAAL.middleware.service.DefaultServiceCaller;
+import org.universAAL.middleware.service.ServiceRequest;
+import org.universAAL.middleware.service.ServiceResponse;
+import org.universAAL.middleware.service.owl.Service;
+import org.universAAL.ontology.location.Location;
+import org.universAAL.ontology.phThing.Device;
+import org.universAAL.ontology.profile.User;
+
+/**
+ * Service Description for checking validity of {@link Session}s.
+ * only the session manager should implement profiles for this service.
+ * @author amedrano
+ *
+ */
+public class SessionManagementService extends Service {
+
+
+    public static final String MY_URI = SecurityOntology.NAMESPACE + "SessionService";
+    public static final String PROP_USER = SecurityOntology.NAMESPACE + "managesSessionsFor";
+	
+	/**
+	 * Only for serializers.
+	 */
+	public SessionManagementService() {
+	    super();
+	}
+
+	/**
+	 * @param uri
+	 */
+	public SessionManagementService(String uri) {
+		super(uri);
+	}
+
+
+	/** {@ inheritDoc}	 */
+	public String getClassURI() {
+		return MY_URI;
+	}
+
+	/** {@ inheritDoc}	 */
+	public boolean isWellFormed() {
+		return super.isWellFormed();
+	}
+
+	/** {@ inheritDoc}	 */
+	public int getPropSerializationType(String propURI) {
+		if (propURI.equals(PROP_USER)){
+			return PROP_SERIALIZATION_FULL;
+		}
+		return PROP_SERIALIZATION_UNDEFINED;
+	}
+
+	/*
+	 * Helper Methods
+	 */
+	
+	/**
+	 * Check whether the given {@link User} has a valid session in the given {@link Device}.
+	 * This check is only bound to the time it is call, session may not be valid in near future.
+	 * @param mc needed to make the Service call.
+	 * @param u the user to be checked
+	 * @param d the device where the user might have a session.
+	 * @return true if and only if the user should be considered to have a valid session (at the moment of the invocation).
+	 */
+	public static boolean hasUserValidSession(ModuleContext mc, User u, Device d){
+	    ServiceRequest sreq = new ServiceRequest();
+	    String usrsOut = SecurityOntology.NAMESPACE + "usersOut";
+	    sreq.addRequiredOutput(usrsOut , new String[]{PROP_USER});
+	    sreq.addValueFilter(new String[]{PROP_USER},  u);
+	    sreq.addValueFilter(new String[]{PROP_USER, SecurityOntology.PROP_SESSION, DeviceBoundSession.PROP_BOUNDED_DEVICE},  d);
+	    ServiceResponse sres = new DefaultServiceCaller(mc).call(sreq);
+	    if (sres == null || !sres.getCallStatus().equals(CallStatus.succeeded)){
+		//By default assume there is no session.
+		return false;
+	    }
+	    List users = sres.getOutput(usrsOut, true);
+	    boolean found = false;
+	    Iterator it = users.iterator();
+	    while (!found && it.hasNext()) {
+		found = ((Resource)it.next()).getURI().equals(u.getURI());
+	    }
+	    return found;
+	}
+	
+	/**
+	 * Check whether the given {@link User} has a valid session in the given {@link Location}.
+	 * This check is only bound to the time it is call, session may not be valid in near future.
+	 * @param mc needed to make the Service call.
+	 * @param u the user to be checked
+	 * @param l the location where the user might have a session.
+	 * @return true if and only if the user should be considered to have a valid session (at the moment of the invocation).
+	 */
+	public static boolean hasUserValidSession(ModuleContext mc, User u, Location l){
+	    ServiceRequest sreq = new ServiceRequest();
+	    String usrsOut = SecurityOntology.NAMESPACE + "usersOut";
+	    sreq.addRequiredOutput(usrsOut , new String[]{PROP_USER});
+	    sreq.addValueFilter(new String[]{PROP_USER},  u);
+	    sreq.addValueFilter(new String[]{PROP_USER, SecurityOntology.PROP_SESSION, LocationBoundSession.PROP_BOUNDED_LOCATION},  l);
+	    ServiceResponse sres = new DefaultServiceCaller(mc).call(sreq);
+	    if (sres == null || !sres.getCallStatus().equals(CallStatus.succeeded)){
+		//By default assume there is no session.
+		return false;
+	    }
+	    List users = sres.getOutput(usrsOut, true);
+	    boolean found = false;
+	    Iterator it = users.iterator();
+	    while (!found && it.hasNext()) {
+		found = ((Resource)it.next()).getURI().equals(u.getURI());
+	    }
+	    return found;
+	}
+}
