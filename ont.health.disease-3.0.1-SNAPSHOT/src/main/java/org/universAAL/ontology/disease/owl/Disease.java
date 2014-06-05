21f3e301fef521418e616b26fca8038d9eb6e0e3
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-06-05 08:16:44 +0000

    Automatic tag of ont.health.disease version: 3.0.1-SNAPSHOT

---
+++ ont.health.disease-3.0.1-SNAPSHOT/src/main/java/org/universAAL/ontology/disease/owl/Disease.java
@@ -0,0 +1,183 @@
+/*******************************************************************************
+ * Copyright 2012 Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology.disease.owl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.universAAL.middleware.owl.ManagedIndividual;
+
+public class Disease extends ManagedIndividual {
+
+    public static final String MY_URI = DiseaseOntology.NAMESPACE + "Disease";
+    public static final String PROP_NAME = DiseaseOntology.NAMESPACE + "name";
+    public static final String PROP_SYMPTHOMS = DiseaseOntology.NAMESPACE
+	    + "sympthoms";
+    public static final String PROP_DISEASE_STATUS = DiseaseOntology.NAMESPACE
+	    + "diseaseStatus";
+    public static final String PROP_ETIOLOGY = DiseaseOntology.NAMESPACE
+	    + "etiology";
+    public static final String PROP_PATOGENY = DiseaseOntology.NAMESPACE
+	    + "patogeny";
+    public static final String PROP_EPIDEMIOLOGY = DiseaseOntology.NAMESPACE
+	    + "epidemiology";
+    public static final String PROP_CONTAGIOUS = DiseaseOntology.NAMESPACE
+	    + "contagious";
+    public static final String PROP_DIAGNOSTIC = DiseaseOntology.NAMESPACE
+	    + "diagnostic";
+    public static final String PROP_PRONOSTIC = DiseaseOntology.NAMESPACE
+	    + "pronostic";
+
+    public Disease() {
+	super();
+    }
+
+    public Disease(String uri) {
+	super(uri);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public int getPropSerializationType(String arg0) {
+	return PROP_SERIALIZATION_FULL;
+    }
+
+    public boolean isWellFormed() {
+	return true && props.containsKey(PROP_NAME)
+		&& props.containsKey(PROP_SYMPTHOMS)
+		&& props.containsKey(PROP_DISEASE_STATUS)
+		&& props.containsKey(PROP_ETIOLOGY)
+		&& props.containsKey(PROP_PATOGENY)
+		&& props.containsKey(PROP_EPIDEMIOLOGY)
+		&& props.containsKey(PROP_CONTAGIOUS)
+		&& props.containsKey(PROP_DIAGNOSTIC)
+		&& props.containsKey(PROP_PRONOSTIC);
+    }
+
+    // GETTERS AND SETTERS
+
+    public String getDiseaseName() {
+	return (String) props.get(PROP_NAME);
+    }
+
+    public void setAssociatedQuestion(String diseaseName) {
+	if (diseaseName != null)
+	    props.put(PROP_NAME, diseaseName);
+    }
+
+    public DiseaseSeverityStatus getDiseaseStatus() {
+	return (DiseaseSeverityStatus) props.get(PROP_DISEASE_STATUS);
+    }
+
+    public void setDiseaseStatus(DiseaseSeverityStatus diseaseStatus) {
+	if (diseaseStatus != null)
+	    props.put(PROP_DISEASE_STATUS, diseaseStatus);
+    }
+
+    public Etiology getEtiology() {
+	return (Etiology) props.get(PROP_ETIOLOGY);
+    }
+
+    public void setEtiology(Etiology etiology) {
+	if (etiology != null)
+	    props.put(PROP_ETIOLOGY, etiology);
+    }
+
+    public Patogeny getPatogeny() {
+	return (Patogeny) props.get(PROP_PATOGENY);
+    }
+
+    public void setPatogeny(Patogeny patogeny) {
+	if (patogeny != null)
+	    props.put(PROP_PATOGENY, patogeny);
+    }
+
+    public Epidemiology getEpidemiology() {
+	return (Epidemiology) props.get(PROP_EPIDEMIOLOGY);
+    }
+
+    public void setEpidemiology(Epidemiology epidemiology) {
+	if (epidemiology != null)
+	    props.put(PROP_EPIDEMIOLOGY, epidemiology);
+    }
+
+    public Diagnostic getDiagnostic() {
+	return (Diagnostic) props.get(PROP_DIAGNOSTIC);
+    }
+
+    public void setDiagnostic(Diagnostic diagnostic) {
+	if (diagnostic != null)
+	    props.put(PROP_DIAGNOSTIC, diagnostic);
+    }
+
+    public Epidemiology getPronostic() {
+	return (Epidemiology) props.get(PROP_EPIDEMIOLOGY);
+    }
+
+    public void setPronostic(Pronostic pronostic) {
+	if (pronostic != null)
+	    props.put(PROP_EPIDEMIOLOGY, pronostic);
+    }
+
+    public boolean isContagious() {
+	Boolean b = (Boolean) props.get(PROP_CONTAGIOUS);
+	return (b == null) ? false : b.booleanValue();
+    }
+
+    public void setIsContagious(Boolean isContagious) {
+	props.put(PROP_CONTAGIOUS, isContagious);
+    }
+
+    public Sympthom[] getSympthoms() {
+
+	Object propList = props.get(PROP_SYMPTHOMS);
+	if (propList instanceof List) {
+	    return (Sympthom[]) ((List) propList).toArray(new Sympthom[0]);
+	} else {
+	    List returnList = new ArrayList();
+	    if (propList != null)
+		returnList.add((Sympthom) propList);
+	    return (Sympthom[]) returnList.toArray(new Sympthom[0]);
+	}
+    }
+
+    public void setSympthoms(Sympthom[] sympthoms) {
+	List propList = new ArrayList(sympthoms.length);
+	for (int i = 0; i < sympthoms.length; i++) {
+	    propList.add(sympthoms[i]);
+	}
+	props.put(PROP_SYMPTHOMS, propList);
+    }
+
+    public void addSympthom(Sympthom sympthom) {
+	Object propList = props.get(PROP_SYMPTHOMS);
+	if (propList instanceof List) {
+	    List list = (List) propList;
+	    list.add(sympthom);
+	    props.put(PROP_SYMPTHOMS, list);
+	} else if (propList == null) {
+	    props.put(PROP_SYMPTHOMS, sympthom);
+	} else {
+	    List list = new ArrayList();
+	    list.add((Sympthom) propList);
+	    list.add(sympthom);
+	    props.put(PROP_SYMPTHOMS, list);
+	}
+    }
+
+}
---
+++ ont.health.disease-3.0.1-SNAPSHOT/src/main/java/org/universAAL/ontology/disease/owl/DiseaseTimeEvolvingStatus.java
@@ -0,0 +1,98 @@
+/*******************************************************************************
+ * Copyright 2012 Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology.disease.owl;
+
+/**
+ * This disease classification depends on the velocity with which the disease evolves
+ * or its duration.
+ */
+
+import org.universAAL.middleware.owl.ManagedIndividual;
+
+public class DiseaseTimeEvolvingStatus extends ManagedIndividual {
+
+    public static final String MY_URI = DiseaseOntology.NAMESPACE
+	    + "DiseaseTimeEvolvingStatus";
+
+    public static final int ACUTE = 0;
+    public static final int CHRONIC = 1;
+
+    private static final String[] names = { "acute", "chronic" };
+
+    public static final DiseaseTimeEvolvingStatus acute = new DiseaseTimeEvolvingStatus(
+	    ACUTE);
+    public static final DiseaseTimeEvolvingStatus chronic = new DiseaseTimeEvolvingStatus(
+	    CHRONIC);
+
+    private int order;
+
+    private DiseaseTimeEvolvingStatus(int order) {
+	super(DiseaseOntology.NAMESPACE + names[order]);
+	this.order = order;
+    }
+
+    public int getPropSerializationType(String propURI) {
+	return PROP_SERIALIZATION_OPTIONAL;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+
+    public String name() {
+	return names[order];
+    }
+
+    public int ord() {
+	return order;
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public static DiseaseTimeEvolvingStatus getTimeEvolvingStatusTypeByOrder(
+	    int order) {
+	switch (order) {
+	case ACUTE:
+	    return acute;
+	case CHRONIC:
+	    return chronic;
+	default:
+	    return null;
+	}
+    }
+    
+    public boolean equals(Object other) {
+		return other instanceof DiseaseTimeEvolvingStatus
+				&& ((DiseaseTimeEvolvingStatus)other).order == order;
+	}
+
+    public static final DiseaseTimeEvolvingStatus valueOf(String name) {
+	if (name == null)
+	    return null;
+
+	if (name.startsWith(DiseaseOntology.NAMESPACE))
+	    name = name.substring(DiseaseOntology.NAMESPACE.length());
+
+	for (int i = ACUTE; i <= CHRONIC; i++)
+	    if (names[i].equals(name))
+		return getTimeEvolvingStatusTypeByOrder(i);
+
+	return null;
+    }
+
+}
