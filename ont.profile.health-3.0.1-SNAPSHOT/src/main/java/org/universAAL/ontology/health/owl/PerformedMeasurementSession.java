3fa0d5887dbd00f23ee95fd2e716f2c657475909
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-06-05 08:17:19 +0000

    Automatic tag of ont.profile.health version: 3.0.1-SNAPSHOT

---
+++ ont.profile.health-3.0.1-SNAPSHOT/src/main/java/org/universAAL/ontology/health/owl/PerformedMeasurementSession.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright 2012 UPM, http://www.upm.es 
+ Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package org.universAAL.ontology.health.owl;
+
+import org.universAAL.ontology.healthmeasurement.owl.HealthMeasurement;
+
+public class PerformedMeasurementSession extends PerformedSession {
+
+    public static final String MY_URI = HealthProfileOntology.NAMESPACE
+	    + "PerformedMeasurementSession";
+
+    public static final String PROP_HAS_HEALTH_MEASUREMENT = HealthProfileOntology.NAMESPACE
+	    + "hasMeasurement";
+
+    public PerformedMeasurementSession() {
+	super();
+    }
+
+    public PerformedMeasurementSession(String uri) {
+	super(uri);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public int getPropSerializationType(String arg0) {
+	return PROP_SERIALIZATION_FULL;
+    }
+
+    public boolean isWellFormed() {
+	return true && props.containsKey(PROP_HAS_HEALTH_MEASUREMENT);
+    }
+
+    // GETTERS & SETTERS
+    public HealthMeasurement getHealthMeasurement() {
+	return (HealthMeasurement) props.get(PROP_HAS_HEALTH_MEASUREMENT);
+    }
+
+    public void setHealthMeasurement(HealthMeasurement hMeasurement) {
+	if (hMeasurement != null)
+	    props.put(PROP_HAS_HEALTH_MEASUREMENT, hMeasurement);
+    }
+
+}
---
+++ ont.profile.health-3.0.1-SNAPSHOT/src/main/java/org/universAAL/ontology/health/owl/WeightMeasurementTreatment.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright 2012 UPM, http://www.upm.es - Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology.health.owl;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.universAAL.ontology.profile.AssistedPersonProfile;
+import org.universAAL.ontology.profile.Caregiver;
+
+public class WeightMeasurementTreatment extends TakeMeasurementActivity {
+
+    public static final String MY_URI = HealthProfileOntology.NAMESPACE
+	    + "WeightMeasurementTreatment";
+
+    public WeightMeasurementTreatment() {
+	super();
+    }
+
+    public WeightMeasurementTreatment(String uri) {
+	super(uri);
+    }
+
+    public WeightMeasurementTreatment(AssistedPersonProfile assistedPerson,
+	    Caregiver caregiver, String tname, String description,
+	    XMLGregorianCalendar stDt, String diseaseURI, WeightRequirement wr) {
+	super(assistedPerson, caregiver, tname, description, stDt, diseaseURI);
+	this.setMeasurementRequirements(wr);
+    }
+
+    public WeightMeasurementTreatment(AssistedPersonProfile assistedPerson,
+	    Caregiver caregiver, String tname, TreatmentPlanning tp,
+	    String description, String diseaseURI, WeightRequirement wr) {
+	super(assistedPerson, caregiver, tname, tp, description, diseaseURI);
+	this.setMeasurementRequirements(wr);
+    }
+
+    public WeightMeasurementTreatment(String tname, String description,
+	    String diseaseURI, WeightRequirement wr) {
+	super(tname, description, diseaseURI);
+	this.setMeasurementRequirements(wr);
+    }
+
+    public WeightMeasurementTreatment(String tname, String description,
+	    XMLGregorianCalendar stDt, String diseaseURI, WeightRequirement wr) {
+	super(tname, description, stDt, diseaseURI);
+	this.setMeasurementRequirements(wr);
+    }
+
+    public WeightMeasurementTreatment(String tname, String description,
+	    TreatmentPlanning tp, String diseaseURI, WeightRequirement wr) {
+	super(tname, description, tp, diseaseURI);
+	this.setMeasurementRequirements(wr);
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public int getPropSerializationType(String arg0) {
+	return PROP_SERIALIZATION_FULL;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+
+}
