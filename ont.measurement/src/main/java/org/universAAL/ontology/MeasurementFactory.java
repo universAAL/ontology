45d603d39dc7a80fb0c2d3a783da65f165dc3324
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-06-05 08:31:24 +0000

    Automatic tag of ont.measurement version: 3.1.0

---
+++ ont.measurement/src/main/java/org/universAAL/ontology/MeasurementFactory.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright 2012 Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology;
+
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.rdf.ResourceFactory;
+import org.universAAL.ontology.measurement.MeanSquareError;
+import org.universAAL.ontology.measurement.Measurement;
+import org.universAAL.ontology.measurement.Signal;
+
+public class MeasurementFactory implements ResourceFactory {
+
+    public Resource createInstance(String classURI, String instanceURI,
+	    int factoryIndex) {
+
+	switch (factoryIndex) {
+	case 0:
+	    return new MeanSquareError(instanceURI);
+	case 1:
+	    return new Measurement(instanceURI);
+	case 2:
+		return new Signal(instanceURI);
+	}
+	return null;
+    }
+}
