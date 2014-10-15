1bb6eef4f901f87fd4b1c835c8eec6a81f80d83e
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-10-15 13:17:16 +0000

    Automatic tag of ont.health.disease version: 3.1.1-SNAPSHOT

---
+++ ont.health.disease-3.1.1-SNAPSHOT/src/main/java/org/universAAL/ontology/disease/DiseaseFactory.java
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * Copyright 2012 Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology.disease;
+
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.rdf.ResourceFactory;
+import org.universAAL.ontology.ICD10CirculatorySystemDiseases.owl.AcuteRheumaticFever;
+import org.universAAL.ontology.ICD10CirculatorySystemDiseases.owl.HeartFailure;
+import org.universAAL.ontology.ICD10CirculatorySystemDiseases.owl.HypertensiveDisease;
+import org.universAAL.ontology.ICD10CirculatorySystemDiseases.owl.IschemicHeartDisease;
+import org.universAAL.ontology.ICD10CirculatorySystemDiseases.owl.OtherCirculatorySystemDisease;
+import org.universAAL.ontology.ICD10Diseases.owl.CertainInfectiousParasiticDisease;
+import org.universAAL.ontology.ICD10Diseases.owl.CirculatorySystemDisease;
+import org.universAAL.ontology.ICD10Diseases.owl.DigestiveSystemDisease;
+import org.universAAL.ontology.ICD10Diseases.owl.MentalDisorder;
+import org.universAAL.ontology.ICD10Diseases.owl.Neoplasms;
+import org.universAAL.ontology.ICD10Diseases.owl.NervousSystemDisease;
+import org.universAAL.ontology.ICD10Diseases.owl.RespiratorySystemDisease;
+import org.universAAL.ontology.disease.owl.Diagnostic;
+import org.universAAL.ontology.disease.owl.Disease;
+import org.universAAL.ontology.disease.owl.Epidemiology;
+import org.universAAL.ontology.disease.owl.Etiology;
+import org.universAAL.ontology.disease.owl.Patogeny;
+import org.universAAL.ontology.disease.owl.Pronostic;
+import org.universAAL.ontology.disease.owl.Sympthom;
+
+public class DiseaseFactory implements ResourceFactory {
+
+    public Resource createInstance(String classURI, String instanceURI,
+	    int factoryIndex) {
+
+	switch (factoryIndex) {
+	case 0:
+	    return new Disease(instanceURI);
+	case 1:
+	    return new Epidemiology(instanceURI);
+	case 2:
+	    return new Etiology(instanceURI);
+	case 3:
+	    return new Patogeny(instanceURI);
+	case 4:
+	    return new Sympthom(instanceURI);
+	case 5:
+	    return new Pronostic(instanceURI);
+	case 6:
+	    return new Diagnostic(instanceURI);
+	case 7:
+	    return new CertainInfectiousParasiticDisease(instanceURI);
+	case 8:
+	    return new CirculatorySystemDisease(instanceURI);
+	case 9:
+	    return new DigestiveSystemDisease(instanceURI);
+	case 10:
+	    return new MentalDisorder(instanceURI);
+	case 11:
+	    return new Neoplasms(instanceURI);
+	case 12:
+	    return new NervousSystemDisease(instanceURI);
+	case 13:
+	    return new RespiratorySystemDisease(instanceURI);
+	case 14:
+	    return new AcuteRheumaticFever(instanceURI);
+	case 15:
+	    return new HeartFailure(instanceURI);
+	case 16:
+	    return new HypertensiveDisease(instanceURI);
+	case 17:
+	    return new IschemicHeartDisease(instanceURI);
+	case 18:
+	    return new OtherCirculatorySystemDisease(instanceURI);
+
+	}
+
+	return null;
+    }
+}
---
+++ ont.health.disease-3.1.1-SNAPSHOT/src/main/java/org/universAAL/ontology/disease/owl/DiseaseSeverityStatus.java
@@ -0,0 +1,102 @@
+/*******************************************************************************
+ * Copyright 2012 Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ontology.disease.owl;
+
+/**
+ * This enumeration allows to classify the disease
+ * depending on the illness severity.  
+ */
+
+import org.universAAL.middleware.owl.ManagedIndividual;
+
+public class DiseaseSeverityStatus extends ManagedIndividual {
+
+    public static final String MY_URI = DiseaseOntology.NAMESPACE
+	    + "DiseaseSeverityStatus";
+
+    public static final int MILD = 0;
+    public static final int SEVERE = 1;
+    public static final int FATAL = 2;
+
+    private static final String[] names = { "mild", "severe", "fatal" };
+
+    public static final DiseaseSeverityStatus mild = new DiseaseSeverityStatus(
+	    MILD);
+    public static final DiseaseSeverityStatus severe = new DiseaseSeverityStatus(
+	    SEVERE);
+    public static final DiseaseSeverityStatus fatal = new DiseaseSeverityStatus(
+	    FATAL);
+
+    private int order;
+
+    private DiseaseSeverityStatus(int order) {
+	super(DiseaseOntology.NAMESPACE + names[order]);
+	this.order = order;
+    }
+
+    public int getPropSerializationType(String propURI) {
+	return PROP_SERIALIZATION_OPTIONAL;
+    }
+
+    public boolean isWellFormed() {
+	return true;
+    }
+
+    public String name() {
+	return names[order];
+    }
+
+    public int ord() {
+	return order;
+    }
+
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public static DiseaseSeverityStatus getSeverityStatusTypeByOrder(int order) {
+	switch (order) {
+	case MILD:
+	    return mild;
+	case SEVERE:
+	    return severe;
+	case FATAL:
+	    return fatal;
+	default:
+	    return null;
+	}
+    }
+    
+    public boolean equals(Object other) {
+		return other instanceof DiseaseSeverityStatus
+				&& ((DiseaseSeverityStatus)other).order == order;
+	}
+
+    public static final DiseaseSeverityStatus valueOf(String name) {
+	if (name == null)
+	    return null;
+
+	if (name.startsWith(DiseaseOntology.NAMESPACE))
+	    name = name.substring(DiseaseOntology.NAMESPACE.length());
+
+	for (int i = MILD; i <= FATAL; i++)
+	    if (names[i].equals(name))
+		return getSeverityStatusTypeByOrder(i);
+
+	return null;
+    }
+
+}
