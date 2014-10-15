33aeefcbfa2b22c190dbe43e9a2a1db4f0923964
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-10-15 13:31:27 +0000

    Automatic tag of ont.impairment version: 3.2.0

---
+++ ont.impairment/src/main/java/org/universAAL/ontology/impairment/ColorBlindness.java
@@ -0,0 +1,61 @@
+/*
+	Copyright 2008-2010 SPIRIT, http://www.spirit-intl.com/
+	SPIRIT S.A. E-BUSINESS AND COMMUNICATIONS ENGINEERING 
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ *//**
+ * 
+ */
+package org.universAAL.ontology.impairment;
+
+import org.universAAL.middleware.owl.supply.LevelRating;
+
+/**
+ * Ontological representation of the color blindness impairment. Methods
+ * included in this class are the mandatory ones for representing an ontological
+ * concept in Java classes for uAAL. Usually it includes getters and setters for
+ * most of its properties.
+ * 
+ * @author mtazari
+ * @author Carsten Stockloew
+ */
+public class ColorBlindness extends SightImpairment {
+    public static final String MY_URI = ImpairmentOntology.NAMESPACE
+	    + "ColorBlindness";
+
+    /**
+     * The constructor for (de-)serializers.
+     */
+    public ColorBlindness() {
+	super();
+    }
+
+    /**
+     * The constructor for use by applications.
+     */
+    public ColorBlindness(LevelRating impairmentLevel) {
+	super(impairmentLevel);
+    }
+
+    /** @see org.universAAL.middleware.owl.ManagedIndividual#getClassURI() */
+    public String getClassURI() {
+	return MY_URI;
+    }
+
+    public String toString() {
+	return "Colorblindness: " + this.getImpaimentLevel().name();
+    }
+}
