a1599d02c8d7ad256195a18356c06e51f3ffa53f
Alejandro M. Medrano Gil <amedrano@lst.tfo.upm.es>
2017-04-11 16:12:41 +0200

    Fixed TestCase, As commented in https://github.com/universAAL/platform/issues/9

--- ont.hvac/src/test/java/org/universAAL/ontology/test/ValuesTest.java
+++ ont.hvac/src/test/java/org/universAAL/ontology/test/ValuesTest.java
@@ -1,12 +1,10 @@
 package org.universAAL.ontology.test;
 
-import org.universAAL.itests.IntegrationTest;
+import org.universAAL.middleware.bus.junit.OntTestCase;
 import org.universAAL.ontology.device.DimmerSensor;
 import org.universAAL.ontology.device.GlassBreakSensor;
 import org.universAAL.ontology.device.HumiditySensor;
 import org.universAAL.ontology.device.StatusValue;
-import org.universAAL.ontology.device.TemperatureLevelSensor;
-import org.universAAL.ontology.device.TemperatureLevelValue;
 
 /**
  * Here developer's of this artifact should code their integration tests.
@@ -14,27 +12,24 @@ import org.universAAL.ontology.device.TemperatureLevelValue;
  * @author rotgier
  * 
  */
-public class ArtifactIntegrationTest extends IntegrationTest {
+public class ValuesTest extends OntTestCase {
 
 
-    public void testComposite() {
-	logAllBundles();
-    }
-    
-    public testValues(){
+   
+    public void testValues(){
 	// This is to test that all Devices can have their values set despite being datatypes or objects
 	// Integers
 	DimmerSensor ds=new DimmerSensor();
-	ds.setHasValue(87);
+	ds.setValue(87);
 	// Floats
 	HumiditySensor hs=new HumiditySensor();
-	hs.setHasValue(8.7f);
+	hs.setValue(8.7f);
 	// StatusValue
 	GlassBreakSensor gs=new GlassBreakSensor();
-	gs.set(StatusValue.Activated);
+	gs.setValue(StatusValue.Activated);
 	// Other *Value
-	TemperatureLevelSensor ts=new TemperatureLevelSensor();
-	ts.setTemperatureLevel(TemperatureLevelValue.HighTemperature);
+//	TemperatureLevelSensor ts=new TemperatureLevelSensor();
+//	ts.set(TemperatureLevelValue.HighTemperature);
     }
 
 }
