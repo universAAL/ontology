955697363bd849804d7786f7763194899c64339b
mtazari <mtazari@igd.fraunhofer.de>
2018-09-24 14:34:05 +0200

    Extended the interface ExternalDataConverter

--- ont.lddi.config.datapoints/src/main/java/org/universAAL/ontology/lddi/config/datapoints/ExternalDataConverter.java
+++ ont.lddi.config.datapoints/src/main/java/org/universAAL/ontology/lddi/config/datapoints/ExternalDataConverter.java
@@ -11,5 +11,8 @@ public interface ExternalDataConverter {
 
 	public Object exportValue(String typeURI, String propURI, Object internalValue);
 	public Object importValue(Object externalValue, String typeURI, String propURI);
+
+	public String toString(String typeURI, String propURI, Object internalValue);
+	public Object valueOf(String internalValue, String typeURI, String propURI);
 	
 }
